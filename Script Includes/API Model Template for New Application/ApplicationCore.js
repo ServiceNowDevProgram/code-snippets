f3ca49bb92202d5a3b7be71826502837e143b38f
Philip Swann <philip.j.swann@gmail.com>
2021-09-30 22:27:30 +0100

    added more meaningful folder naming structure

---
+++ Script Includes/API Model Template for New Application/ApplicationCore.js
@@ -0,0 +1,14 @@
+var ApplicationCore = Class.create();
+ApplicationCore.prototype = Object.extendsObject(ApplicationCoreBase, {
+
+	/** Baseline Application 
+     * Please override any base functionality here 
+     * ---
+     * All vendor changes will be made to the *Base classes
+     * This will ensure you can track changes to future upgrades
+     * 
+     * Copy existing methods to this file and make changes here
+     * **/
+
+    type: 'ApplicationCore'
+});
