e4e6eaa09fe76561e914dc97fe66b776313cbd63
Philip Swann <philip.j.swann@gmail.com>
2021-09-30 20:48:28 +0100

    initial file/folder set up with basic skeleton pattern

---
+++ Script Includes/AppBaselinePattern/ApplicationCore.js
@@ -0,0 +1,14 @@
+var ApplicationCore = Class.create();
+ApplicationCore.prototype = Object.extendsObject(ApplicationCoreBase, {
+
+	/** Baseline Application 
+     * Please override any base functionality here 
+     * ---
+     * All changes will be made to the *Base classes
+     * This will ensure you can easily manage changes to future upgrades
+     * 
+     * Copy existing methods to this file and make changes here
+     * **/
+
+    type: 'ApplicationCore'
+});
---
+++ Script Includes/AppBaselinePattern/ApplicationCoreBase.js
@@ -0,0 +1,52 @@
+var ApplicationCoreBase = Class.create();
+ApplicationCoreBase.prototype = {
+    initialize: function () { },
+
+    /**
+     * Constants for Defaults
+     */
+    C_DEFAULT_NOUN1: gs.getProperty('', ''),
+
+    /**
+     * Constants for Tables
+     */
+    C_TBL_USER: 'sys_user',
+    C_TBL_GROUP: 'sys_user_group',
+
+    /**
+     * Constants for Properties
+     */
+    C_PROP_NOUN1: '',
+
+    /**
+     * get a GlideRecord of the Group based on ID
+     * @param {string} id sys_id of the record
+     * @returns GlideRecord, if it exists
+     */
+    _getGrGroup: function (id) {
+        return this._getGr(this.C_TBL_GROUP, id);
+    },
+
+    /**
+     * get a GlideRecord of the User based on ID
+     * @param {string} id sys_id of the record
+     * @returns GlideRecord, if it exists
+     */
+    _getGrUser: function (id) {
+        return this._getGr(this.C_TBL_USER, id);
+    },
+
+    /**
+     * get a GlideRecord of the specified table
+     * @param {string} strTableName name of table
+     * @param {string} id sys_id of the record
+     * @returns GlideRecord of record, if it exists
+     */
+    _getGr: function (strTableName, id) {
+        var wrGr = new GlideRecord(strTableName);
+        if (!wrGr.get(id)) return;
+        return wrGr;
+    },
+
+    type: 'ApplicationCoreBase'
+};
