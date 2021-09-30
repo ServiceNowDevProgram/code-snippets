de187fc4d92c0a3e4d29944073cfc28203ce3fb4
Pranav Bhagat <pranavbhagat06@gmail.com>
2021-10-01 02:57:57 +0530

    Delete ClientScripts/Make all fields read only directory

--- ClientScripts/Make all fields read only/script.js
+++
@@ -1,8 +0,0 @@
-var fields = g_form.getEditableFields();
-for (var x = 0; x < fields.length; x++) {
-if (fields[x] != 'sys_created_on' && fields[x] != 'sys_created_by' && fields[x] != 'sys_updated_on' && fields[x] != 'sys_updated_by'){
-g_form.setMandatory(fields[x], false);
-g_form.setReadOnly(fields[x], true);
-}
-}
-
