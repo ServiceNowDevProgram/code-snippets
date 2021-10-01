b9b8b39428ef3b253038c3fc6bd67df9e27b6255
Philip Swann <philip.j.swann@gmail.com>
2021-10-01 21:49:56 +0100

    added strategy pattern

---
+++ Script Includes/API Model Template for New Application/Engine.js
@@ -0,0 +1,13 @@
+var Engine = Class.create();
+Engine.prototype = Object.extendsObject(EngineBase, {
+    /** Baseline Application 
+      * Please override any base functionality here 
+      * ---
+      * All vendor changes will be made to the *Base classes
+      * This will ensure you can track changes to future upgrades
+      * 
+      * Copy existing methods to this file and make changes here
+      * **/
+
+    type: 'Engine'
+});
