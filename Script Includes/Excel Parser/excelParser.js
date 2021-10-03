a2884f538b732cbd5eb1ac3c7379b7766e5a59c9
Lacah <47461634+Lacah@users.noreply.github.com>
2021-10-03 18:50:25 +0200

    Create excelParser.js
    Pass in the sys_id of an Excel attachment and get back an array of objects, with every row in the sheet being an element in the array.

---
+++ Script Includes/Excel Parser/excelParser.js
@@ -0,0 +1,28 @@
+var ExcelParser = Class.create();
+ExcelParser.prototype = {
+    initialize: function() {
+    },
+
+    readData: function(attachmentID) {
+        var attID = attachmentID; // depending on client or server side usage
+        //Initialize the GlideSysAttachment call for retrieval of our sheet
+        var attachment = new GlideSysAttachment();
+        var attachmentStream = attachment.getContentStream(attachmentID);
+
+        //Begin to parse the excel sheet for relevant data
+        var parser = new sn_impex.GlideExcelParser();
+        parser.parse(attachmentStream);
+        parser.setHeaderRowNumber(1);
+        var headers = parser.getColumnHeaders(); // Array of headers
+        var excelData = []; // An array that will hold the data from each line as an object
+
+        while (parser.next()) {
+            var row = parser.getRow();
+            if (JSUtil.notNil(row))
+                excelData.push(row);
+        }
+        return excelData;
+    },
+
+    type: 'ExcelParser'
+};
