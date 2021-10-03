794c676540ba6ec4b72b9d5fb070694d8461302b
Irston <irston1491@gmail.com>
2021-10-03 12:50:09 +0530

    Update script.js

--- Business Rules/Allow only unique insert/script.js
+++ Business Rules/Allow only unique insert/script.js
@@ -1,7 +1,7 @@
 (function executeRule(current, previous /*null when async*/) {
 	
 	var gr = new GlideRecord("<table_name>");
-	gr.addQuery("<unique_field>", current.<unique_field_name>);
+	gr.addQuery("<unique_field>", current.<unique_field_name>); // Check for field name uniqueness
 	gr.query();
 	if (gr.next()) {
 		gs.addErrorMessage("Entry for this <field_name> already exists");
