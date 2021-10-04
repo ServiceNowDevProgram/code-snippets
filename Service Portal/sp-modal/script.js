701ba0094efb291ebf5604f8d0728080e303cb4e
Pranav Bhagat <pranavbhagat06@gmail.com>
2021-10-04 15:04:24 +0530

    Update script.js

--- Service Portal/sp-modal/script.js
+++ Service Portal/sp-modal/script.js
@@ -4,11 +4,11 @@ var c = this;
 
 var html = "<p>Test</p>";
 // Use spModal to pop the HTML
-	spModal.open({
-		title: 'This is test Modal: ',
-		message: html,
-		buttons: [
-			{label:'OK', primary: true}
-		]
-	});
+spModal.open({
+	title: 'This is test Modal: ',
+	message: html,
+	buttons: [
+		{label:'OK', primary: true}
+	]
+});
 };
