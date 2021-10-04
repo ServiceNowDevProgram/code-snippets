38c3102a9f55b08d0de344396e4c6820f4229386
Philip Swann <philip.j.swann@gmail.com>
2021-10-04 21:23:10 +0100

    strategy files

---
+++ Script Includes/API Model Template for New Application/ExampleStrategy2.js
@@ -0,0 +1,12 @@
+var ExampleStrategy2 = Class.create();
+ExampleStrategy2.prototype = Object.extendsObject(ExampleStrategy2Base, {
+    /** Baseline Application 
+    * Please override any base functionality here 
+    * ---
+    * All vendor changes will be made to the *Base classes
+    * This will ensure you can track changes to future upgrades
+    * 
+    * Copy existing methods to this file and make changes here
+    * **/
+    type: 'ExampleStrategy2'
+});
