8ef5eb108f6564eb4ec2bb2cc66b77dedd63af0c
Ishaan S <50629150+ishaanshoor@users.noreply.github.com>
2021-10-04 22:33:46 +1100

    Update requested_item_approval_reminder_requestor.js

--- Scheduled Jobs/Approval Reminder/requested_item_approval_reminder_requestor.js
+++ Scheduled Jobs/Approval Reminder/requested_item_approval_reminder_requestor.js
@@ -1,4 +1,3 @@
-var count = 0;
 var dateTime = new GlideDateTime();
 var currentDate = dateTime.getLocalDate();
 var grSA = new GlideAggregate('sysapproval_approver');
@@ -8,5 +7,4 @@ grSA.query();
 while (grSA.next()) {  
 	         var current = grSA.sysapproval.getRefRecord(); 
            gs.eventQueue('event.name',current,current.requested_for); //Add your own event name to trigger the notification.
-  	       count++;
 }
