d6f78157966b67a6f11293bf8b11263d5ba139bb
ReginaldFawcett <91907396+ReginaldFawcett@users.noreply.github.com>
2021-10-04 15:49:54 -0400

    Update quartersAgo.js

--- GlideSystem/date-time/quartersAgo.js
+++ GlideSystem/date-time/quartersAgo.js
@@ -3,6 +3,6 @@ gs.info('Now: ' + now);
 var quartersAgo = gs.quartersAgo(2); //Returns a date and time for a certain number of quarters ago
 gs.info('2 Quarters Ago: ' + quartersAgo);
 
-Output Example:
+//Output Example:
 //*** Script: Now: 2021-10-04 15:41:32
 //*** Script: 2 Quarters Ago: 2021-04-04 15:41:32
