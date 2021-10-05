785f0361171eef045e7d85a33033da4ee5254d7a
Ishaan S <50629150+ishaanshoor@users.noreply.github.com>
2021-10-06 06:52:52 +1100

    Update create_catalog_item_from_email.js

--- Inbound Actions/Advanced Scripts/create_catalog_item_from_email.js
+++ Inbound Actions/Advanced Scripts/create_catalog_item_from_email.js
@@ -7,8 +7,8 @@ function createRequest() {
    var item = cart.addItem('SYS_ID');   //Add sys_id of the catag item 
 
 // set variables
-var worker = email.body.exiting_worker_name;
-var termination = email.body.termination_date;
+var worker = email.body.name;
+var termination = email.body.date;
 var term_f = new GlideDateTime();
 term_f.setDisplayValue(termination);
 var userid = email.body.user_id;
@@ -20,16 +20,8 @@ if (userRec.next()) {
    var userID = userRec.getUniqueValue();
 }
 
-var locRec = new GlideRecord("cmn_location");
-locRec.addQuery("name", location);
-locRec.query();
-if (locRec.next()) {
-var	loc = locRec.getUniqueValue();
-}
-
 cart.setVariable(item, 'employee', userID);
 cart.setVariable(item, 'employee_termination_date', term_f);
-cart.setVariable(item, 'location', loc);
 
 var rc = cart.placeOrder();   
 var ritmSys = rc.number;
