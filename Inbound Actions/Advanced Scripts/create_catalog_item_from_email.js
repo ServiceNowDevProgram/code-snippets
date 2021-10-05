374747d7aa72fd4be38ca7f004efa297f387003a
Ishaan S <50629150+ishaanshoor@users.noreply.github.com>
2021-10-06 06:45:31 +1100

    Update create_catalog_item_from_email.js

--- Inbound Actions/Advanced Scripts/create_catalog_item_from_email.js
+++ Inbound Actions/Advanced Scripts/create_catalog_item_from_email.js
@@ -4,17 +4,14 @@ function createRequest() {
 
    var cart = new Cart();   //calling the cart API
 
-   var item = cart.addItem('7adc4a75dbeb989047c0f132f39619cb');   //sys_id of the catalog item I want to fire
+   var item = cart.addItem('SYS_ID');   //Add sys_id of the catag item 
 
-	// set variables
+// set variables
 var worker = email.body.exiting_worker_name;
-var manager = email.body.manager_name;
 var termination = email.body.termination_date;
 var term_f = new GlideDateTime();
 term_f.setDisplayValue(termination);
-var location = email.body.exiting_worker_country;
-var sso = email.body.exiting_worker_sso_number;
-var userid = sso + "@latitudefs.com";
+var userid = email.body.user_id;
 
 var userRec = new GlideRecord("sys_user");
 userRec.addQuery("user_name", userid);
@@ -22,29 +19,22 @@ userRec.query();
 if (userRec.next()) {
    var userID = userRec.getUniqueValue();
 }
-var mgrRec = new GlideRecord("sys_user");
-mgrRec.addQuery("name", manager);
-mgrRec.query();
-if (mgrRec.next()) {
- var mngr = mgrRec.getUniqueValue();
+
+var locRec = new GlideRecord("cmn_location");
+locRec.addQuery("name", location);
+locRec.query();
+if (locRec.next()) {
+var	loc = locRec.getUniqueValue();
 }
-	var locRec = new GlideRecord("cmn_location");
-	locRec.addQuery("name", location);
-	locRec.query();
-	if (locRec.next()) {
-	var	loc = locRec.getUniqueValue();
-	}
 
 cart.setVariable(item, 'employee', userID);
-cart.setVariable(item, 'employee_manager', mngr);
 cart.setVariable(item, 'employee_termination_date', term_f);
 cart.setVariable(item, 'location', loc);
 
 var rc = cart.placeOrder();   
 var ritmSys = rc.number;
 
-updateRITM(rc.sys_id);   //call a function immediately to update the ritm.   This must be a nested function, otherwise inbound actions get weird.
-           
+updateRITM(rc.sys_id);   //call a function immediately to update the ritm.           
 }
 
 function updateRITM(req){
