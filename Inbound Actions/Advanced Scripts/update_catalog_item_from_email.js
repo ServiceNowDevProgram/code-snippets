44e4e57c3fb3c8d353e45f7c988c45d5b33dafbf
Ishaan S <50629150+ishaanshoor@users.noreply.github.com>
2021-10-06 06:55:03 +1100

    Update update_catalog_item_from_email.js

--- Inbound Actions/Advanced Scripts/update_catalog_item_from_email.js
+++ Inbound Actions/Advanced Scripts/update_catalog_item_from_email.js
@@ -1,8 +1,7 @@
-var worker = email.body.exiting_worker_name;
-var termination = email.body.termination_date;
+var worker = email.body.name;
+var termination = email.body.date;
 var term_f = new GlideDateTime();
 term_f.setDisplayValue(termination);
-var location = email.body.exiting_worker_location;
 var userid = email.body.user_id;
 
 var userRec = new GlideRecord("sys_user");
@@ -13,13 +12,13 @@ if (userRec.next()) {
 }
 
 var ritm = new GlideRecord('sc_req_item');
-var eq = 'cat_item=7adc4a75dbeb989047c0f132f39619cb^active=true^request.requested_for='+ userID+'^variables.342d8e75dbeb989047c0f132f39619fa='+userID;
+var eq = 'cat_item=SYS_ID_OF_THE_ITEM^active=true^request.requested_for='+ userID;
 ritm.addEncodedQuery(eq);
 ritm.query();
 if(ritm.next()){
 	ritm.work_notes = "received from: " + email.origemail + "\n\n" + email.body_text;
 	ritm.variables.employee_termination_date = term_f;
-    ritm.description = email.body_text;  
+    	ritm.description = email.body_text;  
 	new Workflow().restartWorkflow(ritm);
     ritm.update();
 }
