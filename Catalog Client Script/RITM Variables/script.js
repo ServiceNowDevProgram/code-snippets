1a919314817dcc87dd3b5710d22ea88feb3c3bed
Sumanth Dosapati <sumanthdosapati3@gmail.com>
2021-10-06 22:01:36 +0530

    Update script.js

--- Catalog Client Script/RITM Variables/script.js
+++ Catalog Client Script/RITM Variables/script.js
@@ -3,6 +3,6 @@ function onLoad()
   
    g_form.setVariablesReadOnly(true); //if you want to make all variables read-only
   
-   g_form.setVariablesReadOnly(true); //if you want to make all variables editable
+   g_form.setVariablesReadOnly(false); //if you want to make all variables editable
   
 }
