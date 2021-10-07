c7b58aab4d1d04634eed37c7f1a35a8f17a59ca1
Sumanth Dosapati <sumanthdosapati3@gmail.com>
2021-10-07 21:05:26 +0530

    Control all RITM variables in one go (#161)
    * Code snippet that validates special characters
    
    * Code snippet that validates special characters
    
    * Code that controls all RITM variables in single go
    
    * create readme.md
    
    * Update script.js
    
    * Updated folder name
    
    * Updated readme.md
    
    * Create ui_script.js
    
    * Create onLoad.js
    
    * Create readme.md
    
    * Update readme.md
    
    * Delete readme.md
    
    * Create readme.md
    
    * Delete UI Scripts/Display number of created records directory
    
    Co-authored-by: Earl Duque <31702109+earlduque@users.noreply.github.com>

---
+++ Catalog Client Script/Control all RITM variables in one go/script.js
@@ -0,0 +1,8 @@
+function onLoad()
+{
+  
+   g_form.setVariablesReadOnly(true); //if you want to make all variables read-only
+  
+   g_form.setVariablesReadOnly(false); //if you want to make all variables editable
+  
+}
