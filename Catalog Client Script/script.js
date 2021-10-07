56c1ded35d6deb1ac11833356999fd37da0891a1
azeezgaa <48519253+azeezgaa@users.noreply.github.com>
2021-10-07 16:54:06 -0500

    Delete script.js

--- Catalog Client Script/script.js
+++
@@ -1,8 +0,0 @@
-function onLoad() {
-//Type appropriate comment here, and begin script below
-	
-var glideURL = new GlideURL();
-glideURL.setFromCurrent();
-var id = glideURL.getParam("sysparm_id"); // provide the parameter name
-	
-}
