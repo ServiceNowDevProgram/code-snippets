9cb8f8619a1fa4804c90173624192f8ed8f70b59
azeezgaa <48519253+azeezgaa@users.noreply.github.com>
2021-10-07 16:54:54 -0500

    Create script.js

---
+++ Catalog Client Script/Get URL Parameters/script.js
@@ -0,0 +1,8 @@
+function onLoad() {
+//Type appropriate comment here, and begin script below
+	
+var glideURL = new GlideURL();
+glideURL.setFromCurrent();
+var id = glideURL.getParam("sysparm_id"); // provide the parameter name
+	
+}
