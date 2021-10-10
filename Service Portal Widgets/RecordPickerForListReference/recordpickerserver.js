140df99d25d043c1a5ca89b87ee84ea0daa79fff
Alikutty Abdulrazak <47845869+alikuttyka@users.noreply.github.com>
2021-10-10 21:31:42 +0530

    Update recordpickerserver.js

--- Service Portal Widgets/RecordPickerForListReference/recordpickerserver.js
+++ Service Portal Widgets/RecordPickerForListReference/recordpickerserver.js
@@ -4,7 +4,7 @@
         var changedUserName = input.userId;
         var changedUserId = input.userName;
         /**Add any logic here to update and return the response**/
-		//data.response = "";
+	//data.response = "";
         //console.log("userName: " + changedUserName);
         //console.log("userId: " + changedUserId);
     } else {
