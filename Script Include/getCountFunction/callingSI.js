1e02cd9db088bfbc5bc973552df3cbea332be39f
ashu759 <33026500+ashu759@users.noreply.github.com>
2021-10-10 20:58:52 +0200

    Calling ScriptInclude for getCount

---
+++ Script Include/getCountFunction/callingSI.js
@@ -0,0 +1,20 @@
+var IncidentSI = Class.create();
+IncidentSI.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+    type: "IncidentSI",
+    generic: new GenericNOW(), 
+/**
+     * Retrieve the number of active (non closed) incident tasks
+     * @param {sys_id} id - The id of an incident
+     * @returns {integer} - The number of open incident tasks
+     */
+    getActiveIncTasksCount: function(id) {
+        // Return the count of all open incident tasks associated with the incident
+        var result = this.generic.count({
+            table: "incident_task",
+            query: "active=true^incident=" + id
+        });
+        if (result.error) {
+            gs.error("getActiveIncTasksCount => Error message: " + result.error);
+        }
+        return result.count;
+    },
