94f0a8bc879fc8465c34ec2c98f96538103c5285
Pranav Bhagat <pranavbhagat06@gmail.com>
2021-10-11 20:14:33 +0530

    Create script.js

---
+++ Server Side/Phone Number formating/script.js
@@ -0,0 +1,21 @@
+
+// Format Phone Number in standard format 4-3-3 example if phone is  1234567891  then output will be 1234 567 891
+
+
+function normalize(phone) {
+  //normalize string and remove all unnecessary characters
+  phone = phone.replace(/[^\d]/g, "");
+  //check if number length equals to 10
+  if (phone.length == 10) {
+      //reformat and return phone number
+      return phone.replace(/(\d{4})(\d{3})(\d{3})/, "$1 $2 $3");
+  }
+
+  return null;
+}
+
+//Example for Background scripts
+var k = '1234567891';
+k = k.substring(0, 10);
+var phone = normalize(k);
+gs.info(phone)
