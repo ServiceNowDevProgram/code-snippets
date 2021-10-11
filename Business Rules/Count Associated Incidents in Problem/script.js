fdfbe714c79c7fe60e78d1c72234d7b3e26090dd
Pratyush M <42929557+pratyushmp@users.noreply.github.com>
2021-10-12 01:12:25 +0530

    Update script.js

--- Business Rules/Count Associated Incidents in Problem/script.js
+++ Business Rules/Count Associated Incidents in Problem/script.js
@@ -1,15 +1,15 @@
-(function executeRule(current, previous /*null when async*/) {
+(function executeRule(current, previous) {
 
-	// Add your code here
+	
 	var gr = new GlideRecord('problem');
-// 	gr.initialize();
 	gr.addQuery('sys_id', current.problem_id);
 	gr.query();
 	if(gr.next())
 		{
 			gr.u_associated_incidents += 1;
+			gr.update();
 			
 		}
-	gr.update();
+	
 	
 })(current, previous);
