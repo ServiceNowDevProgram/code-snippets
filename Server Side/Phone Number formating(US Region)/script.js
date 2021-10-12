8cc8bc70e16e3547f69bc729312c78b23f7be43a
Pranav Bhagat <pranavbhagat06@gmail.com>
2021-10-13 01:21:31 +0530

    Update script.js

--- Server Side/Phone Number formating(US Region)/script.js
+++ Server Side/Phone Number formating(US Region)/script.js
@@ -8,7 +8,7 @@ function normalize(phone) {
   //check if number length equals to 10
   if (phone.length == 10) {
       //reformat and return phone number
-      return phone.replace(/(\d{4})(\d{3})(\d{3})/, "$1 $2 $3");
+      return phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
   }
 
   return null;
