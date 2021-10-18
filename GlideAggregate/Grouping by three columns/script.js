10116aea49aaaf1cda461929e347354cf028f03c
Jakub <13350956+R3J3NT@users.noreply.github.com>
2021-10-18 19:50:46 +0200

    Removed un-needed nesting

--- GlideAggregate/Grouping by three columns/script.js
+++ GlideAggregate/Grouping by three columns/script.js
@@ -1,49 +1,25 @@
 //GlideAggregate script for grouping by three columns
 //In this case it is displaying the number of active changes grouped by state, type and priority
 
-//First GlideAggregate query which is aggregated by state
-var gaChangeState = new GlideAggregate('change_request');
-gaChangeState.addActiveQuery();
+var gaChange = new GlideAggregate('change_request');
+gaChange.addAggregate('COUNT', 'state');
 
-//Aggregate by the first selected field
-gaChangeState.addAggregate('count', 'state');
+//Chained orderBy allows to group by mutliply columns
+gaChange.orderBy('state');
+gaChange.orderBy('type');
+gaChange.orderBy('priority');
 
-gaChangeState.query();
+gaChange.addActiveQuery();
+gaChange.query();
 
-while (gaChangeState.next()) {
+while (gaChange.next()) {
+    //Getting values of fields needed to logging 
+    var ChangeCount = gaChange.getAggregate('COUNT', 'state');
+    var state = gaChange.getDisplayValue('state');
+    var type = gaChange.getValue('type');
+    var priority = gaChange.getDisplayValue('priority');
 
-    //Second GlideAggregate query which is aggregated by type		
-    var gaChangeType = new GlideAggregate('change_request');
-    gaChangeType.addActiveQuery();
+    //Logging information about the number of changes in group of state, type and priority
+    gs.info('Active changes in state: [' + state + '] with type: [' + type + '] and priority: [' + priority + '] = ' + ChangeCount);
 
-    //It is important to query by fields used in previous GlideAggregates
-    gaChangeType.addQuery('state', gaChangeState.state);
-
-    //Aggregate by second selected field
-    gaChangeType.addAggregate('count', 'type');
-
-    gaChangeType.query();
-
-    while (gaChangeType.next()) {
-
-        //Third GlideAggregate query which is aggregated by priority		
-        var gaChangePriority = new GlideAggregate('change_request');
-        gaChangePriority.addActiveQuery();
-
-        //It is important to query by fields used in previous GlideAggregate
-        gaChangePriority.addQuery('state', gaChangeState.state);
-        gaChangePriority.addQuery('type', gaChangeType.type);
-
-        //Aggregate by third selected field
-        gaChangePriority.addAggregate('count', 'priority');
-
-        gaChangePriority.query();
-
-        //Going through all combinations of state, type and priority
-        while (gaChangePriority.next()) {
-
-            //Logging information about the number of changes in group of state, type and priority
-            gs.info('Active changes in state: [' + gaChangeState.state.getDisplayValue() + '] with type: [' + gaChangeType.type.getDisplayValue() + '] and priority: [' + gaChangePriority.priority.getDisplayValue() + '] = ' + gaChangePriority.getAggregate('count', 'priority'));
-        }
-    }
 }
