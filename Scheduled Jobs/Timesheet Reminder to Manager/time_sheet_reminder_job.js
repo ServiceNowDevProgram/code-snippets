cfb941305589262f285d45c13c00d04175892219
Ishaan S <50629150+ishaanshoor@users.noreply.github.com>
2021-10-20 04:33:16 +1100

    Update time_sheet_reminder_job.js

--- Scheduled Jobs/Timesheet Reminder to Manager/time_sheet_reminder_job.js
+++ Scheduled Jobs/Timesheet Reminder to Manager/time_sheet_reminder_job.js
@@ -1,8 +1,8 @@
 var ga = new GlideAggregate('time_sheet_exception');
-ga.addEncodedQuery('stateINPending,Submitted,Rejected,Not_Submitted^week_starts_onONLast week@javascript:gs.beginningOfLastWeek()@javascript:gs.endOfLastWeek()');
+ga.addEncodedQuery('stateINPending,Submitted,Rejected,Not_Submitted^week_starts_onONLast week@javascript:gs.beginningOfLastWeek()@javascript:gs.endOfLastWeek()'); //Change the query as per your requirement. 
 ga.addAggregate('count','user.manager'); 
 ga.query();
 while(ga.next())
 {
-gs.eventQueue('timesheet.manager',current,ga.getValue('user.manager'),ga.getValue('user.manager'));
+gs.eventQueue('timesheet.manager',current,ga.getValue('user.manager'));  
 }
