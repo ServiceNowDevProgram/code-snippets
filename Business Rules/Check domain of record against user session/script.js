480ddd075c57ff91d2d0a0cb80a0ba751d1ef14e
Dean Croaker <dean.croaker@gmail.com>
2021-10-20 13:02:57 +1100

    added better Domain name representation for session domain

--- Business Rules/Check domain of record against user session/script.js
+++ Business Rules/Check domain of record against user session/script.js
@@ -8,7 +8,7 @@
 	if (cd.name.nil()) {
 		g_scratchpad.currentDomain = 'Global';
 	} else {
-		g_scratchpad.currentDomain = cd.name;
+		g_scratchpad.currentDomain = cd.sys_domain.getDisplayValue();
 	}
 	
 	//set the scratchpad value to the domain of the record it's called from
