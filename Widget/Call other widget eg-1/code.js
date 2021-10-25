40b74f8233ca3d298438fea1732e3bb9d358fa55
MihirRathod <46079154+MihirRathod@users.noreply.github.com>
2021-10-25 17:26:07 +0530

    Call widget in current widget Example 1

---
+++ Widget/Call other widget eg-1/code.js
@@ -0,0 +1,6 @@
+//Call a widget inside your widget
+//HTML
+<div>
+ <h1>This is Parent Widget<h1>
+    <widget id='my_child_widget_1'></widget> // 'Child Widget ID' must be the widget you want to call in current widget
+</div>
