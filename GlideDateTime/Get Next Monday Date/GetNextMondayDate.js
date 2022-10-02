80e741a72f927bb807c9116ad5b1eb4a875890f0
Daniel Maldonado <danmax2006@gmail.com>
2022-10-02 18:37:40 -0400

    Update log message
    Update log message with useful information about the next upcoming Monday date.

--- GlideDateTime/Get Next Monday Date/GetNextMondayDate.js
+++ GlideDateTime/Get Next Monday Date/GetNextMondayDate.js
@@ -5,7 +5,7 @@ function GetNextMonday() {
     
     var nextMonday = (today - dayInWeek + 8); //8 for Monday of Next week. 
     date.addDaysLocalTime(nextMonday); // interger of days until next monday
-    
+    gs.info("The next upcoming Monday is on " + date.getDate() + ", in "+ nextMonday + " days.");
     return date.getDate();
 
 }
