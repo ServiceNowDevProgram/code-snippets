ec0e68f67e3ba0a40ceb1c9a6064da723126f595
Shakeel Shaik <48235492+shakeel-sheikh@users.noreply.github.com>
2022-10-14 20:35:57 +0530

    Update call_script_include.js

--- Mail Scripts/Call Script Include in Notification Mail Script/call_script_include.js
+++ Mail Scripts/Call Script Include in Notification Mail Script/call_script_include.js
@@ -2,20 +2,12 @@
           /* Optional EmailOutbound */ email, /* Optional GlideRecord */ email_action,
           /* Optional GlideRecord */ event) {
   
+   //Call Global Application - Script Include in Mail script
+    var getReturnfromGlobalSI = new global.ScriptIncludeName().functionName();
+    template.print(getReturnfromGlobalSI);
+          
    //Call Scoped Application - Script Include in Mail script
-    var portalSuffix = new sn_ex_emp_fd.FoundationNotificationUtil().getPortalSuffix();
-  template.print(portalSuffix);
-   
-   //Call UI Message in Mail script
-    var buttonText = gs.getMessage('View request');
-  template.print(portalSuffix);
-    
-  //Call Global Application - Script Include in Mail script
-    var requestNotificationJs = new global.RequestNotificationUtil();
-    requestNotificationJs.createNotificationPrimayAction(template, requestUrl, buttonText);
- 
-   //Call System Property in Mail script
-    var priceDisplay = gs.getProperty('glide.sc.price.display');
-     
+    var getReturnfromScopedSI = new x_scope_name.ScriptIncludeName().functionName();
+    template.print(getReturnfromScopedSI);
    
 })(current, template, email, email_action, event);
