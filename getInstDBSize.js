2aeb7a88e8dccd38b3ea9cf24bc71d9679e34e37
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-10-14 10:56:26 -0700

    Delete getInstDBSize.js

--- getInstDBSize.js
+++
@@ -1,6 +0,0 @@
-gs.info(
-  SNC.UsageAnalyticsScriptUtils.getCount(
-    'Primary DB size (MB)',
-    'Primary DB size of this instance (in MB)'
-  )
-); //Run this code in the Background Scripts module to get the size of the DB
