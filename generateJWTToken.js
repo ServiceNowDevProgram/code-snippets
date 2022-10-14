d25fdfe6ed5cb8abad44cd7547b6ce2c743ae2e6
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-10-14 10:17:32 -0700

    Delete generateJWTToken.js

--- generateJWTToken.js
+++
@@ -1,8 +0,0 @@
-var jwtAPI = new sn_auth.GlideJWTAPI();
-var headerJSON = {  "kid": "<key id value>"  };
-var header = JSON.stringify(headerJSON);
-var payloadJSON = { "iss": "<client_id value>", "sub": "ravikumar@gmail.com", "aud”:”<client_id value>”, ”exp”:”36500” };
-var payload = JSON.stringify(payloadJSON);
-var jwtProviderSysId = "<JWT provider sys id value>";
-var jwt = jwtAPI.generateJWT(jwtProviderSysId, header, payload);
-gs.info("******JWT Token:" + jwt);
