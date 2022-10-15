bcbf60d507cb37c57b896fd83f021cd95c235a2b
Sören Maucher <soeren_maucher@web.de>
2022-10-15 10:52:23 +0200

    Update script.js

--- UI Actions/Call Subflow/script.js
+++ UI Actions/Call Subflow/script.js
@@ -1,20 +1,20 @@
 (function() {
 	
 	try {
-    // Provide subflow inputs
+    		// Provide subflow inputs
 		var inputs = {};
 		inputs['incident'] = current; //the current incident record can be provided as input to the subflow
     
-    //The subflow can either be executed synchronously (running in foreground) or asynchronously (running in background)
-    //If the flow runs asynchronously the FlowAPI will not provide the outputs of the subflow
+    		//The subflow can either be executed synchronously (running in foreground) or asynchronously (running in background)
+    		//If the flow runs asynchronously the FlowAPI will not provide the outputs of the subflow
     
-    //Asynchronous call
+    		//Asynchronous call
 		// sn_fd.FlowAPI.getRunner().subflow('global.subflow_name').inBackground().withInputs(inputs).run();
     
-    //Synchronous call
+    		//Synchronous call
 		// sn_fd.FlowAPI.getRunner().subflow('global.create_problem_from_incident').inForeground().withInputs(inputs).run();
     
-    //In this case we are calling the subflow global.create_problem_from_incident synchronously and then access the subflow outputs
+    		//In this case we are calling the subflow global.create_problem_from_incident synchronously and then access the subflow outputs
 		var result = sn_fd.FlowAPI.getRunner().subflow('global.create_problem_from_incident').inForeground().withInputs(inputs).run();
 		var outputs = result.getOutputs();
 
