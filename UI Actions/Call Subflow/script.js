49c0d437b8b9ba59e835ce65689668fb28f32c5e
Sören Maucher <soeren_maucher@web.de>
2022-10-15 11:07:50 +0200

    Update script.js

--- UI Actions/Call Subflow/script.js
+++ UI Actions/Call Subflow/script.js
@@ -12,7 +12,7 @@
 		// sn_fd.FlowAPI.getRunner().subflow('global.subflow_name').inBackground().withInputs(inputs).run();
     
     		//Synchronous call
-		// sn_fd.FlowAPI.getRunner().subflow('global.create_problem_from_incident').inForeground().withInputs(inputs).run();
+		// sn_fd.FlowAPI.getRunner().subflow('global.subflow_name').inForeground().withInputs(inputs).run();
     
     		//In this case we are calling the subflow global.create_problem_from_incident synchronously and then access the subflow outputs
 		var result = sn_fd.FlowAPI.getRunner().subflow('global.create_problem_from_incident').inForeground().withInputs(inputs).run();
