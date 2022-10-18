0a131476047dae45ea4f58861ed9d2ae8904af1f
Mahesh-Krishnan1 <89046716+Mahesh-Krishnan1@users.noreply.github.com>
2022-10-18 15:09:52 -0700

    Update data_xform_from_attachment.js

--- Business Rules/Transform Data from Attachment/data_xform_from_attachment.js
+++ Business Rules/Transform Data from Attachment/data_xform_from_attachment.js
@@ -1,6 +1,7 @@
 (function executeRule(current, previous /*null when async*/ ) {
+	
 
-	var file_load_gr = new GlideRecord('x_cpts_onboarding_amzn_wkly_dsp_file_load');
+	var file_load_gr = new GlideRecord('name_of_table_to_house_loaded_file');
 	file_load_gr.addQuery('sys_id', current.sys_id);
 	file_load_gr.query();
 	if (file_load_gr.next()) {
@@ -9,9 +10,10 @@
 		return;
 	}
 
-	//update current records file name with name of attached file
+	//update filename field on custom table with name of attached file
+	
 	var attachment = new GlideSysAttachment();
-	var agr = attachment.getAttachments('x_cpts_onboarding_amzn_wkly_dsp_file_load', file_load_gr.sys_id.toString());
+	var agr = attachment.getAttachments('name_of_table_to_house_loaded_file', file_load_gr.sys_id.toString());
 	if (agr.next()) {
 		//has an attachment and found it
 		var LoadFileName = agr.getValue('file_name');
@@ -19,10 +21,10 @@
 		file_load_gr.file_name = LoadFileName;
 		file_load_gr.update();
 
-		var importSetTableName = "x_cpts_onboarding_amazon_weekly_dsp_load";
-		var transformMapIDs = "62591fe11bbb6c1053bfa93be54bcb18"; //Amazon weekly DSP file load
-		var applicatonScope = "CPTS Onboarding";
-		var dataSourceName = 'Amazon weekly DSP load';
+		var importSetTableName = "name_of_importset_table";
+		var transformMapIDs = "sys_id of transform map"; //Amazon weekly DSP file load
+		var applicatonScope = "name of application scope"; //if creating this inside a specific scope or else Global
+		var dataSourceName = 'name_of_dataource';
 		var dataSourceID = null;
 
 		var dataSource = new GlideRecord('sys_data_source');
@@ -34,13 +36,13 @@
 			while (attachList.next()) {
 				attach.deleteAttachment(attachList.getValue('sys_id'));
 			}
-			attach.copy('x_cpts_onboarding_amzn_wkly_dsp_file_load', file_load_gr.sys_id.toString(), 'sys_data_source', dataSource.sys_id.toString());
+			attach.copy('name_of_table_to_house_loaded_file', file_load_gr.sys_id.toString(), 'sys_data_source', dataSource.sys_id.toString());
 			dataSourceID = dataSource.sys_id;
 		}
 
 		/*
 		// Schedule Load of Attachment
-		// This script below initiates the data load from the attachment
+		// This Script Include below initiates the data load from the attachment and is provided by SN
 		*/
 
 		new global.EmailFileImportUtil().scheduleImport(dataSourceID, transformMapIDs);
