422e11bc5232798891490ecf66c14e80d7503c82
Sören Maucher <soeren_maucher@web.de>
2022-10-20 14:34:30 +0200

    Create script.js

---
+++ Script Includes/Get Field Label in Specific Language/script.js
@@ -0,0 +1,29 @@
+var LanguageUtils = Class.create();
+LanguageUtils.prototype = {
+  
+    getLabel: function(table, variable, language) {
+        var grLabel = new GlideRecord("sys_documentation");
+        grLabel.addQuery("name", table);
+        grLabel.addQuery("element", variable);
+        grLabel.addQuery("language", language);
+        grLabel.query();
+        if (grLabel.next()) {
+            return grLabel.getValue("label");
+        } else {
+            var grEnLabel = new GlideRecord("sys_documentation");
+            grEnLabel.addQuery("name", table);
+            grEnLabel.addQuery("element", variable);
+            grEnLabel.addQuery("language", "en"); //default language English
+            grEnLabel.query();
+            if (grEnLabel.next()) {
+                gs.log("The variable " + variable + " does not have a label in " + language + " associated to it, in the following table: +" + table);
+                return grEnLabel.getValue("label");
+            } else {
+                gs.log("The variable " + variable + " can not be found in the following table: +" + table);
+                return;
+            }
+        }
+    },
+
+    type: 'LanguageUtils'
+};
