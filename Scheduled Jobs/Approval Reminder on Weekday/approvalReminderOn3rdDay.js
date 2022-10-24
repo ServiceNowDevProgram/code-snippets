ec7934575adfea4e929d2f23a6a488391dd3c0e5
Radhe-Manasa <manasakodali.cse@gmail.com>
2022-10-24 20:21:14 +0530

    Update approvalReminderOn3rdDay.js

--- Scheduled Jobs/Approval Reminder on Weekday/approvalReminderOn3rdDay.js
+++ Scheduled Jobs/Approval Reminder on Weekday/approvalReminderOn3rdDay.js
@@ -1,5 +1,5 @@
 //Trigger notification reminder on weekday to approvals if they not approved in 3 days  
-var mk=new GlideRecord('sysapproval_approver');
+var mk=new GlideAggregate('sysapproval_approver');
 mk.addEncodedQuery('source_table=sc_req_item^state=requested');
 //fectch the requested approval of request item table
 mk.query();   
@@ -10,12 +10,12 @@ var gr1 = gs.nowDateTime();
 var stdate=new GlideDateTime(mk.getValue('sys_updated_on'));
 //gs.print(stdate + "  start date" );
 var days=parseInt('9');
-days=days*3
+days=days*3;
 gs.print(days);
 var dur=new GlideDuration(60*60*1000*days);
 gs.print(dur + ":duration");
 var sch=new GlideSchedule('08fcd0830a0a0b2600079f56b1adb9ae');
-//weekday schedule sys is
+//weekday schedule sys id
 var end=sch.add(stdate,dur);
 var diff= parseInt((gs.dateDiff(gr1, end,true))/60/60/24);
 if(diff<=0)
