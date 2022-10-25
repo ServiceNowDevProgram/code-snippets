81c5a49190376599a261bd8c6c6b262f9d268e62
Radhe-Manasa <manasakodali.cse@gmail.com>
2022-10-26 00:54:48 +0530

    Update 90daysInactiveScript.js

--- Inactive Records/90daysInactiveScript.js
+++ Inactive Records/90daysInactiveScript.js
@@ -1,5 +1,17 @@
- var id=current.sys_id;
-var tableName=current.sys_class_name;          
+//Formatted the code for background script       
+var grIncident = new GlideRecord('incident');
+grIncident.addEncodedQuery("sys_created_onONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()^active=true");
+grIncident.orderByDesc('number');
+//grIncident.setLimit(20);
+grIncident.query();
+while (grIncident.next()) {
+
+    var id=grIncident.getValue('sys_id');
+    var tableName=grIncident.getValue('sys_class_name');
+
+    // var id="e329de99731423002728660c4cf6a73c";
+//var tableName="incident";  
+
               var gdt = new GlideDateTime(gs.nowDateTime());
              gdt.addDays(90);
           
@@ -24,7 +36,7 @@ var tableName=current.sys_class_name;
               grST.trigger_type = 0;
 
               grST.insert();
-           
+           }
 
               function doTimelySchedule(id) {
 
@@ -44,10 +56,11 @@ var tableName=current.sys_class_name;
 
                                            + "}";
 
-                                           return ret;                                       
+                            return ret;      
+                           gs.print( "return value : " + ret);//prints the inserted code in sysy trigger table
+                           //open the schedule item with the name 
+                                                            
 
-                                          
 
                              }
-
                             
