2dc64d6c512e1c014683c8c4a31ed1858fe1d87e
Techiadarshrock <adarshsingh0205@gmail.com>
2022-10-26 14:01:37 -0400

    Optimized Code

--- Advanced Conditions/Group Approval Check/group_approval_check.js
+++ Advanced Conditions/Group Approval Check/group_approval_check.js
@@ -1,8 +1,9 @@
 (function () {
-    if (!JSUtil.nil(current.group)) { // is there a group approval record?
+    var grp = current.group;
+    if (!JSUtil.nil(grp)) { // is there a group approval record?
         var grg = new GlideRecord("sysapproval_approver");
-        grg.addQuery('group', current.group); // if so, check for other approvers in the same group
-        grg.addQuery('state', 'approved'); // look for approved records
+        grg.addQuery('group', grp); // if so, check for other approvers in the same group
+        grg.addQuery('state', '=', 'approved'); // look for approved records
         grg.query();
         if (grg.hasNext()) { // if any records exist
             return false; // return false, don't send an email
