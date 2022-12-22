b1d99c6a3b5f956e1c4a97809d358bba751ee526
mskoddow <58812581+mskoddow@users.noreply.github.com>
2022-12-22 10:07:49 +0100

    Update CatalogUtils.js
    Complete redesign to handle MVRS

--- Script Includes/CatalogUtils/CatalogUtils.js
+++ Script Includes/CatalogUtils/CatalogUtils.js
@@ -8,80 +8,104 @@ var CatalogUtils = Class.create();
  * @author Maik Skoddow
  */
 CatalogUtils.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
-    /**
+	/**
 	 * Returns all variables of a given RITM as stringified array of JSON objectss.
 	 *
 	 * @param {Object} objParameter Valid `sc_req_item` record or String-based Sys ID of a RITM.
 	 * @return {String} Stringified array of JSON objects with various variable values and information or an empty array if no valid data could be found.
 	 */
-    getVariables: function(objParameter) {
-        var _grRITM = null;
+	getVariables: function(objParameter) {
+		var _grRITM = null;
 
-        //server-side call with Sys ID
-        if (typeof objParameter == 'string' && objParameter.length == 32) {
-            _grRITM = new GlideRecord('sc_req_item');
-            
-            if (!_grRITM.get(objParameter)) {
-                _grRITM = null;
-            } 
-		    }
+		//server-side call with Sys ID
+		if (typeof objParameter == 'string' && objParameter.length == 32) {
+			_grRITM = new GlideRecord('sc_req_item');
 
-        //server-side call with initialized RITM GlideRecord 
-        if (typeof objParameter == 'object' && objParameter instanceof GlideRecord) {
-            if (objParameter.isValidRecord() && objParameter.getTableName() == 'sc_req_item') {
-                _grRITM = objParameter;
-            }
-        }
+			if (!_grRITM.get(objParameter)) {
+				_grRITM = null;
+			} 
+		}
 
-        //client-side Ajax call with RITM Sys ID as parameter
-        if (this.getParameter('sysparm_ritm_sys_id')) {
-            var _strSysID = String(this.getParameter('sysparm_ritm_sys_id')).trim();
+		//server-side call with initialized RITM GlideRecord 
+		if (typeof objParameter == 'object' && objParameter instanceof GlideRecord) {
+			if (objParameter.isValidRecord() && objParameter.getTableName() == 'sc_req_item') {
+				_grRITM = objParameter;
+			}
+		}
 
-            if (_strSysID.length == 32) {
-                _grRITM = new GlideRecord('sc_req_item');
-            
-                if (!_grRITM.get(_strSysID)) {
-                    _grRITM = null;
-                }
-            }
-        }
+		//client-side Ajax call with RITM Sys ID as parameter
+		if (this.getParameter('sysparm_ritm_sys_id')) {
+			var _strSysID = String(this.getParameter('sysparm_ritm_sys_id')).trim();
 
-        //no valid RITM could be loaded
-        if (_grRITM == null) {
-            return '[]';
-        }
+			if (_strSysID.length == 32) {
+				_grRITM = new GlideRecord('sc_req_item');
 
-        //could be improved by offering a configuration method for excluded variable types
-        var _strExcludedTypes = '|11|12|14|15|17|19|20|24|'; 
-        var _arrResult        = [];
-        var _grVariables      = new GlideRecord('sc_item_option_mtom');
+				if (!_grRITM.get(_strSysID)) {
+					_grRITM = null;
+				}
+			}
+		}
 
-        //load all catalog variables for given RITM
-        _grVariables.addQuery('request_item', _grRITM.sys_id);
-        _grVariables.orderBy('sc_item_option.order');
-        _grVariables.query();
+		//no valid RITM could be loaded
+		if (_grRITM == null) {
+			return '[]';
+		}
 
-        while (_grVariables.next()) {
-            var _strType         = _grVariables.sc_item_option.item_option_new.type.toString();
-            var _strName         = _grVariables.sc_item_option.item_option_new.name.toString();
-            var _strQuestionText = _grVariables.sc_item_option.item_option_new.question_text.toString();
-            var _strValue        = _grVariables.sc_item_option.value.toString();
-            var _strDisplayValue = _grRITM.getDisplayValue('variables.' + _strName);
+		var _arrResult                            = [];
+		var _strExcludedGlideElementVariableTypes = '|11|12|14|15|17|19|20|24|'; 
+		var _grGlideElementVariables              = new GlideRecord('sc_item_option_mtom');
+		var _objGlideElementVariables             = {};
 
-            //if type is not excluded fill result array with variable values
-            if (_strExcludedTypes.indexOf('|' + _strType + '|') == -1) {
-                _arrResult.push({
-                    strType:         _strType,
-                    strName:         _strName,
-                    strQuestionText: _strQuestionText,
-                    strValue:        _strValue,
-                    strDisplayValue: _strDisplayValue,
-                });
-            }
-        }
+		//load all catalog variables for a given RITM
+		_grGlideElementVariables.addQuery('request_item', _grRITM.getValue('sys_id'));
+		_grGlideElementVariables.query();
 
-        return JSON.stringify(_arrResult);
-    },
+		while (_grGlideElementVariables.next()) {
+			var _strType         = _grGlideElementVariables.sc_item_option.item_option_new.type.toString();
+			var _strName         = _grGlideElementVariables.sc_item_option.item_option_new.name.toString();
+			var _strQuestionText = _grGlideElementVariables.sc_item_option.item_option_new.question_text.toString();
+			var _strValue        = _grGlideElementVariables.sc_item_option.value.toString();
+			var _strDisplayValue = _grRITM.getDisplayValue('variables.' + _strName);
 
-    type: 'CatalogUtils',
+			//if type is not excluded fill result array with variable values
+			if (_strExcludedGlideElementVariableTypes.indexOf('|' + _strType + '|') == -1) {
+				_objGlideElementVariables[_strName] = {
+					strType:         _strType,
+					strName:         _strName,
+					strQuestionText: _strQuestionText,
+					strValue:        _strValue,
+					strDisplayValue: _strDisplayValue,
+				};
+			}  
+		}
+
+
+		for (var _strKey in _grRITM.variables) {
+			var _objVariable     = _grRITM.variables[_strKey];
+			var _strVariableType = Object.prototype.toString.call(_objVariable).match(/^\[object\s(.*)\]$/)[1];
+
+			//is it a real catalog variable?
+			if (_strVariableType == 'GlideElementVariable') {
+				//is the catalog variable available?
+				if (_objGlideElementVariables[_strKey]) {
+					_arrResult.push(_objGlideElementVariables[_strKey]);
+				}
+			}
+
+			//is it a MVRS?
+			if (_strVariableType == 'TableVariableNode') {
+				_arrResult.push({
+					strType:         'MVRS',
+					strName:         _strKey,
+					strQuestionText: _grRITM.variables[_strKey].getLabel(),
+					strValue:        _grRITM.variables[_strKey].toString(),
+					strDisplayValue: null,      
+				});
+			}  
+		}
+
+		return JSON.stringify(_arrResult, '', 4);
+	},
+
+	type: 'CatalogUtils',
 });
