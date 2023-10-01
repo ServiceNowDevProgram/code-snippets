f8cd7a50d8c0a509baf4b711161f1dbbc7f79001
RagaChitraMankala <146649033+RagaChitraMankala@users.noreply.github.com>
2023-10-01 22:54:04 +0530

    Update and rename On Change to On Change.js

---
+++ Client Scripts/On Change.js
@@ -0,0 +1,22 @@
+
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+    if (isLoading) {
+        return;
+    }
+
+    //Type appropriate comment here, and begin script below
+    var reason = g_form.getValue('hold_reason');
+    var state_val = g_form.getValue('state');
+    //Checking if the substate is with customer and the worknotes is not empty
+
+    if (reason == 1 && state_val == 3) {
+
+        if (newValue != '') {
+            g_form.setMandatory('work_notes', false);
+        }
+        //Checking if the worknotes is empty
+        else if (newValue == '') {
+            g_form.setMandatory('work_notes', true);
+        }
+    } 
+}
