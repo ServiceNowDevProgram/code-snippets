9eea452b4f4ac650e76fd96815ac5cac71dc5226
Debendu Das <66923140+debendu-das@users.noreply.github.com>
2023-10-02 20:25:38 +0530

    Client Script - Add Field Message Add Field Message.js

---
+++ Client Scripts/Add Field Message/Add Field Message.js
@@ -0,0 +1,5 @@
+function onLoad() {
+    // Adding Field Messages
+    g_form.showFieldMsg("caller_id", "This is the Requester", "info");
+	  g_form.showFieldMsg("short_description", "Please provide a short description", "info");
+}
