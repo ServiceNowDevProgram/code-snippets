44d8fbe0e4db9edf62dca02d3061e3f254736482
riya-misra-1 <146907130+riya-misra-1@users.noreply.github.com>
2023-10-04 14:42:49 +0530

    Update checkUserRole.js

--- Flow Actions/check userRole/checkUserRole.js
+++ Flow Actions/check userRole/checkUserRole.js
@@ -1,13 +1,13 @@
 (function execute(inputs, outputs) {
-  var roleGR = new GlideRecord("sys_user_has_role");
-  roleGR.addQuery("user", inputs.userid);
+  var roleGR = new GlideRecord("sys_user_has_role"); // querying sys_user_has_role record
+  roleGR.addQuery("user", inputs.userid); // finding the particular user in the record
   roleGR.query();
 
   outputs.user_has_role = false;
 
   while (roleGR.next()) {
-    if (roleGR.role.name == inputs.role) {
-      outputs.user_has_role = true;
+    if (roleGR.role.name == inputs.role) { //checking if the role of the user is same as the role provided in the flow 
+      outputs.user_has_role = true; //setting user_has_role to true
     }
   }
 })(inputs, outputs);
