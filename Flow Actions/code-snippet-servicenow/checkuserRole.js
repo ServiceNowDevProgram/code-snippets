a990ce9c8464661931027db7715ab34b8d818b49
riya-misra-1 <146907130+riya-misra-1@users.noreply.github.com>
2023-10-04 14:28:16 +0530

    Update checkuserRole.js

--- Flow Actions/code-snippet-servicenow/checkuserRole.js
+++ Flow Actions/code-snippet-servicenow/checkuserRole.js
@@ -1,1 +1,14 @@
+(function execute(inputs, outputs) {
+  var roleGR = new GlideRecord("sys_user_has_role");
+  roleGR.addQuery("user", inputs.userid);
+  roleGR.query();
+
+  outputs.user_has_role = false;
+
+  while (roleGR.next()) {
+    if (roleGR.role.name == inputs.role) {
+      outputs.user_has_role = true;
+    }
+  }
+})(inputs, outputs);
 
