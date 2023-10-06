ac97dd520540e0359f9c52c7eecb97c1d6c647ec
niamccash <39105458+niamccash@users.noreply.github.com>
2023-10-06 13:17:01 -0400

    Fixed indentation

--- Catalog Client Script/Currency Validation/currency_validation.js
+++ Catalog Client Script/Currency Validation/currency_validation.js
@@ -2,13 +2,13 @@ function onChange(control, oldValue, newValue, isLoading) {
     if (isLoading || newValue == '') {
         return;
     }
-	
+
     var cost = g_form.getValue('variable_name'); //update variable name used for currency
     cost = cost.trim();
     // first character should be dollar sign
     var firstChar = cost.substring(0, 1);
     if (firstChar != '$') {
-		validationAlert(oldValue);
+        validationAlert(oldValue);
     }
 
     // characters after the $ sign should be numerics
@@ -26,15 +26,15 @@ function onChange(control, oldValue, newValue, isLoading) {
     // there must be 2 digits only after the decimal
     var decNum = num.substring(num.indexOf('.') + 1, num.length);
     if (decNum.length != 2) {
-		validationAlert(oldValue);
+        validationAlert(oldValue);
     }
 }
 
 function validationAlert(oldValue) {
-		g_form.setValue("variable_name", oldValue);
-        var gm = new GlideModal("glide_warn");
-        gm.setTitle("Currency formatting problem");
-        gm.setPreference("title", "Please enter cost in $0.00 format");
-        gm.render();
-		return;
+    g_form.setValue("variable_name", oldValue);
+    var gm = new GlideModal("glide_warn");
+    gm.setTitle("Currency formatting problem");
+    gm.setPreference("title", "Please enter cost in $0.00 format");
+    gm.render();
+    return;
 }
