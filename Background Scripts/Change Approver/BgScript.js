fbfd8d0dd559360fd20141eb216f96b96f83152d
Jaspal <30924269+Jaspalsinghchot@users.noreply.github.com>
2023-10-07 18:58:44 +0400

    BgScript.js

--- Background Scripts/Change Approver/BgScript.js
+++ Background Scripts/Change Approver/BgScript.js
@@ -1,1 +1,9 @@
 
+var ab=new GlideRecord('sysapproval_approver');
+ab.addQuery('sysapproval','<Approval_record_sys_id>');
+ab.query();
+if(ab.next())
+{
+ab.approver.setDisplayValue('<User Display Name here>')';
+ab.update();
+}
