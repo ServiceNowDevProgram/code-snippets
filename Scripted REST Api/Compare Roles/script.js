8ad4add4669f08dae1ac060ddf99a22eb9283ba9
riya-misra-1 <146907130+riya-misra-1@users.noreply.github.com>
2023-10-09 16:39:51 +0530

    Update script.js in compare-roles

--- Scripted REST Api/Compare Roles/script.js
+++ Scripted REST Api/Compare Roles/script.js
@@ -15,19 +15,19 @@
     var differences = [];
 
     var fieldsGR = new GlideRecord('sys_dictionary'); //querying over dictionary record
-    fieldsGR.addQuery('name', 'sys_user_role'); //querying over particular user table in dictionary record
+    fieldsGR.addQuery('name', 'sys_user_role'); //querying over particular role table in dictionary record
     fieldsGR.query();
     while (fieldsGR.next()) {
         var fieldName = fieldsGR.element.getDisplayValue();
         if (role1GR[fieldName] && role2GR[fieldName]) {
-            var role1Value = role1GR.getDisplayValue(fieldName);  //fetching the value of field for user1
-            var role2Value = role2GR.getDisplayValue(fieldName);  //fetching the value of field for user2
+            var role1Value = role1GR.getDisplayValue(fieldName);  //fetching the value of field for role1
+            var role2Value = role2GR.getDisplayValue(fieldName);  //fetching the value of field for role2
             if (role1Value !== role2Value) { // checking the difference
                 differences.push({
                     field: fieldsGR.column_label.getDisplayValue(),
                     role1Value: role1Value,
                     role2Value: role2Value
-                }); //stroing different value between two user in differences array
+                }); //stroing different value between two role in differences array
             }
         }
     }
