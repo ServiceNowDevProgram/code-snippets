a38becf02d8857742bdc1d0dfb1300a17c36a1a1
Karthiga Selvaraj <50877672+karthiga-sk@users.noreply.github.com>
2023-10-09 19:42:04 +0530

    Create check_index.js
    Given scripts will take any input value and convert it into a String then make it lowercase to find the occurring string. If a match isn't found it will return -1.

---
+++ Script Includes/Check IndexOf value of any input/check_index.js
@@ -0,0 +1,2 @@
+var input = "test@edu.in"; // Any input value string, array or etc
+gs.info(input.toString().toLowerCase.indexOf("edu")); // Returns the position of occuring match in numeric. for the given example the script returns 5
