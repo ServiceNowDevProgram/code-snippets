754e261887a2c7126971fe5ff304ced76f8656f6
Carleen Carter <40255402+smartcarleen@users.noreply.github.com>
2023-10-11 09:15:28 -0500

    Update getMyDirectReports.js
    This looping script traverses the User table from a certain point to get either one level of employees or all employees in the hierarchy underneath the logged-on user. There are two functions:
    1. **getMyDirectReports**: gets only users directly reporting to the logged on user
    1. **getMyReports**: gets all users reporting to the logged on user

--- Business Rules/getMyDirectReports/getMyDirectReports.js
+++ Business Rules/getMyDirectReports/getMyDirectReports.js
@@ -1,50 +1,62 @@
-function getMyDirectReports(mgrID) {
-    var au = new ArrayUtil();
-
-    if (typeof mgrID == "undefined") {
-        mgrID = gs.getUserID();
-    }
-    var myPeople = [];
-
-    var u = new GlideRecord("sys_user");
-    u.addQuery("manager", mgrID);
-    u.addQuery("sys_id", "!=", mgrID);
-    u.addQuery("active", true);
-    u.query();
-    while (u.next()) {
-        if (!au.contains(myPeople, u.sys_id)) {
-            myPeople.push(u.sys_id + "");
-        }
+function getMyDirectReports(managerID) {
+    //if no User sys_id is passed in, assume we need the direct reports for the logged on user.
+    if (typeof managerID == "undefined") {
+        managerID = gs.getUserID();
     }
 
+    //unique method is used to ensure no duplicates
+    var myPeople = []; //array for direct reports sys_ids
+
+    var userGr = new GlideRecord("sys_user");
+    userGr.addQuery("manager", managerID);
+    userGr.addQuery("sys_id", "!=", managerID); //recursion protection, exclude the manager
+    userGr.addQuery("active", true); //only active employees
+    userGr.query();
+    if (!userGr.next()) {
+        //return early if no direct reports
+        return myPeople;
+    }
+    while (userGr.next()) {
+        myPeople.push(userGr.getUniqueValue());
+    }
+    
+    // remove duplicates
+    var arrayUtil = new ArrayUtil();
+    arrayUtil.unique(myPeople);
+    
     return myPeople;
 }
 
-function getMyReports(mgrID) {
-    var au = new ArrayUtil();
-
-    if (typeof mgrID == "undefined") {
-        mgrID = gs.getUserID();
+function getMyReports(managerID) {
+    //if no User sys_id is passed in, assume we need the direct reports for the logged on user.
+    if (typeof managerID == "undefined") {
+        managerID = gs.getUserID();
     }
-    var myReports = [];
-    getReportsRecursive(mgrID, myReports);
 
-    au.unique(myReports);
+    var arrayUtil = new ArrayUtil(); //unique method is used to ensure no duplicates
+    var myReports = []; //array for direct reports sys_ids
+
+    getReportsRecursive(managerID, myReports);
+
+    arrayUtil.unique(myReports);
     return myReports;
 }
 
-function getReportsRecursive(mgrID, myReports) {
-    var au = new ArrayUtil();
+function getReportsRecursive(managerID, myReports) {
+    var arrayUtil = new ArrayUtil();
 
-    var u = new GlideRecord("sys_user");
-    u.addQuery("manager", mgrID);
-    u.addQuery("sys_id", "!=", mgrID);
-    u.addQuery("active", true);
-    u.query();
-    while (u.next()) {
-        if (!au.contains(myReports, u.sys_id)) {
-            myReports.push(u.sys_id + "");
-            getReportsRecursive(u.sys_id, myReports);
+    var userGr = new GlideRecord("sys_user");
+    userGr.addQuery("manager", managerID);
+    userGr.addQuery("sys_id", "!=", managerID); //recursion protection 1, exclude the manager
+    userGr.addQuery("active", true); //only active employees
+    userGr.query();
+    while (userGr.next()) {
+        var userID = userGr.getUniqueValue();
+        //recursion protection 2, only proceed with a new search if this user isn't already in the array.
+        if (!arrayUtil.contains(myReports, userID)) {
+            myReports.push(userID);
+            //keep following the org hierarchy until we don't find any more reports
+            getReportsRecursive(userID, myReports);
         }
     }
 }
