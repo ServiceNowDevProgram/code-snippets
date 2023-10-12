d1b2acc07e7f278bb21e68952b4c5c97d267560d
Mohit Kaushik <33430400+kmohit2809@users.noreply.github.com>
2023-10-12 12:41:01 +0530

    Update glide_confirm.js

--- GlideModal/Confirm Message/glide_confirm.js
+++ GlideModal/Confirm Message/glide_confirm.js
@@ -1,6 +1,6 @@
 var gm = new GlideModal("glide_confirm", false, 600); // glide_confirm is the OOB prebuilt UI page.
-gm.setTitle("Test title"); // The title for your confirm message box
-gm.setPreference("title", "Test body text"); // this will be the body for your confirm message
+gm.setTitle("Conform box title"); // The title for your confirm message box
+gm.setPreference("title", "confirm box body"); // this will be the body for your confirm message
 gm.setPreference("warning", "false"); // put it true if you want to display a warning sign on your confirm box
 gm.setPreference("onPromptSave", function() {alert("You clicked on 'Save'")});
 gm.setPreference("onPromptDiscard", function() {alert("You clicked on 'Don't save'")});
