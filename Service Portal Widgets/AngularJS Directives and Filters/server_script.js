0ea3cfba36c90e7559b4661f70a5fe616159c6be
Ashoo Jindal <94731401+ashoo-jindal@users.noreply.github.com>
2023-10-16 20:22:06 +0530

    AngularJS directives and filters (#872)
    * Create readme.md
    
    Description of widget.
    
    * Update readme.md
    
    * Update readme.md
    
    * Update readme.md
    
    * Create html.html
    
    * Create css.css
    
    * Create client_script.js
    
    * Create server_script.js

---
+++ Service Portal Widgets/AngularJS Directives and Filters/server_script.js
@@ -0,0 +1,9 @@
+(function() {
+  /* populate the 'data' object */
+  /* e.g., data.table = $sp.getValue('table'); */
+
+	//Variables from Server Script
+	data.record = [1,2,3,4,5,6];	
+	data.record1 = ["abc","xyz","Abc","Xyz","ABC","XYZ"];
+
+})();
