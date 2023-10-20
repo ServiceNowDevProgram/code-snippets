08f67d3cf028eacff5eb3b1329b2843b725b24cb
Abhishekchowdarapu <60147157+Abhishekchowdarapu@users.noreply.github.com>
2023-10-20 17:47:23 +0530

    Update deleteattachment.js

--- Attachments/Delete RITM Attachment/deleteattachment.js
+++ Attachments/Delete RITM Attachment/deleteattachment.js
@@ -1,6 +1,3 @@
-// To automatically delete RITM attachment, when an attachment is deleted on SCTASK.
-// Create a After Business Rule, and set business rule condition as Table name is "sctask", so that when an attachment is deleted on SCTASK, this business rule will run.
-
 var gr = new GlideRecord("sys_attachment");
     var task = new GlideRecord('sc_task');
     if (task.get(current.table_sys_id)) {
