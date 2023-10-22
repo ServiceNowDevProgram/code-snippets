750d64864ddff7d04a2a1614f6ef958db72cfc91
Abhishekchowdarapu <60147157+Abhishekchowdarapu@users.noreply.github.com>
2023-10-22 21:03:16 +0530

    Update Hide Variables.js

--- Catalog Client Script/Hide Variables of Catalog Item on Order Guide/Hide Variables.js
+++ Catalog Client Script/Hide Variables of Catalog Item on Order Guide/Hide Variables.js
@@ -1,5 +1,6 @@
 function onLoad(){
   if(g_service_catalog.isOrderGuide()){
-    g_form.setDisplay('varaible_name1,false);
-    g_form.setDisplay('varaible_name2,false);
+    //variable_name1, varaible_name2 are the fields already present on the Order guide, hence hiding below fields on the catalog form when the catalog form is used through an order guide.
+    g_form.setDisplay('varible_name1',false);
+    g_form.setDisplay('varible_name2',false);
   }
