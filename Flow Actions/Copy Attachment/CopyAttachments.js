9bcccd1b5dc79ff91a761ced0a02548832d7e290
NBaker137 <95248857+NBaker137@users.noreply.github.com>
2023-10-24 17:15:35 -0500

    Create CopyAttachments.js

---
+++ Flow Actions/Copy Attachment/CopyAttachments.js
@@ -0,0 +1,4 @@
+(function execute(inputs, outputs) {
+// Be sure to update variable names below to match your inputs
+  GlideSysAttachment.copy(inputs.originalTable, inputs.originalSysId, inputs.newTable, inputs.newSysId);
+})(inputs, outputs);
