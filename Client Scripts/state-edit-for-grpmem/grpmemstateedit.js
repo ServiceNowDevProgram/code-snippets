4a2c241f74a2982c627b4022129e1068c6e0e7f2
TVani5 <30842729+TVani5@users.noreply.github.com>
2023-10-29 20:34:40 +0100

    Update grpmemstateedit.js

--- Client Scripts/state-edit-for-grpmem/grpmemstateedit.js
+++ Client Scripts/state-edit-for-grpmem/grpmemstateedit.js
@@ -1,15 +1,11 @@
-//Declare g_scartchpad.grpmember in the display BusinessRule of the table on which you would like to have the state editable only for group members.
-
 //Create a Display Business Rule with the following code and then the following code on "On-Load" client script
 
 //Display Business Rule Code:
 /*****
-
-g_scratchpad.grpmember = gs.getUser().isMemberOf(current.assignment_group);
-
+g_scratchpad.grpmember = gs.getUser().isMemberOf(current.assignment_group); //This returns true or false . If user is part of the group it returns true, if user is not part of the group it returns false and assign it to scratchpad variable.
 *****/
 
-if(g_scratchpad.grpmember == true)
+if(g_scratchpad.grpmember == false)
 {
-  g_form.setReadonly("state", true);
+g_form.setReadonly("state", true);
 }
