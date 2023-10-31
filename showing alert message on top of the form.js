3c4912b55d4dfa7b2b660a58d7f44c5926c084c4
Nikhil Kumar Kamlekar <139611736+Nikuuuuu@users.noreply.github.com>
2023-11-01 02:05:31 +0530

    Create showing alert message on top of the form.js
    Showing alert message on form when states change as per script

---
+++ showing alert message on top of the form.js
@@ -0,0 +1,15 @@
+function onCellEdit(sysIDs, table, oldValues, newValue, callback) {
+  var saveAndClose = true;
+	if(new value == 6);//Resolved
+	{
+		alert("user can't change state to 'Resolved from a list'");
+		saveAndClose=false;
+	}
+	if(new value == 7);//Closed
+	{
+		alert("user can't change state to 'clsoed from a list'");
+	saveAndClsoe=false;
+	}
+
+ callback(saveAndClose); 
+}
