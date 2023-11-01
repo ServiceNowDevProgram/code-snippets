bb051705826595e350225891b14503ea0bb7300f
ANUBHAV RITOLIA <anubhavritolia@gmail.com>
2023-11-01 17:03:31 +0530

    Update listofchildIncident.js
    This code will display all the Child Incident Numbers for the particular Incident.

--- GlideRecord/List of Child Incidents/listofchildIncident.js
+++ GlideRecord/List of Child Incidents/listofchildIncident.js
@@ -1,8 +1,9 @@
+var parentID = "9c573169c611228700193229fff72400"; // This should be stored in sys_property instead of hardcoding the sys_id. 
 var inc = new GlideRecord('incident');
-inc.addEncodedQuery("parent_incident=9c573169c611228700193229fff72400"); // sys_id of parent Incident
+inc.addEncodedQuery('parent_incident='+parentID); // sys_id of parent Incident
 inc.query();
 
-gs.info("List of Child Incidents of INC0000001:");
+gs.info('List of Child Incidents of INC0000001:');
 
 while(inc.next())
 {
