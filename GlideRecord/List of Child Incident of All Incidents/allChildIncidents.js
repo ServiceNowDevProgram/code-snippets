9bdeeebf3116e3b502e193a03857a74940a3c90d
ANUBHAV RITOLIA <anubhavritolia@gmail.com>
2023-11-01 19:08:16 +0530

    Update allChildIncidents.js

--- GlideRecord/List of Child Incident of All Incidents/allChildIncidents.js
+++ GlideRecord/List of Child Incident of All Incidents/allChildIncidents.js
@@ -1,13 +1,12 @@
 var inc = new GlideAggregate('incident');
-inc.addEncodedQuery('parent_incidentISNOTEMPTY');
+inc.addEncodedQuery('parent_incidentISNOTEMPTY'); // This encoded query can be modified to more condition based on requirement.
 inc.groupBy('parent_incident');
 inc.addAggregate('COUNT');
 inc.query();
 
-//gs.info("List of Child Incidents of INC0000001:");
 
 while(inc.next())
 {
-	gs.info("Number of Child Incident of "+ inc.getDisplayValue('parent_incident')+ " are " +inc.getAggregate('COUNT')+ ".");
+	gs.info('Number of Child Incident of '+ inc.getDisplayValue('parent_incident')+ ' are ' +inc.getAggregate('COUNT')+ '.');
 }
 
