35c69166b7604e625c33fd41bb2e90060ecfada7
ANUBHAV RITOLIA <anubhavritolia@gmail.com>
2023-11-01 19:05:51 +0530

    Update allChildIncidents.js
    Updated the code.

--- GlideRecord/List of Child Incident of All Incidents/allChildIncidents.js
+++ GlideRecord/List of Child Incident of All Incidents/allChildIncidents.js
@@ -1,20 +1,13 @@
-var inc = new GlideRecord('incident');
-inc.addEncodedQuery(query); // You can add your filter query to check it for particular list of Incidents like Department wise, Last created on 30 days, etc.
-inc.setLimit(x); // If you want you can limit the records to some extend to avoid performance issues.
+var inc = new GlideAggregate('incident');
+inc.addEncodedQuery('parent_incidentISNOTEMPTY');
+inc.groupBy('parent_incident');
+inc.addAggregate('COUNT');
 inc.query();
+
+//gs.info("List of Child Incidents of INC0000001:");
 
 while(inc.next())
 {
-	var childInc = new GlideRecord('incident');
-	childInc.addQuery('parent_incident',inc.getUniqueValue());
-	childInc.query();
-
-if(childInc.hasNext()) {
-	gs.info("Number of Child Incident of "+inc.number + " are " +childInc.getRowCount()+ ". List of Child Incident numbers are as below:");
+	gs.info("Number of Child Incident of "+ inc.getDisplayValue('parent_incident')+ " are " +inc.getAggregate('COUNT')+ ".");
 }
-	while(childInc.next())
-	{
-		gs.info(childInc.getValue('number'));
 
-	}
-}
