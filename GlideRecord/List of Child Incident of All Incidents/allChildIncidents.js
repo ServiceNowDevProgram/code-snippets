682752c550987be4c4fd7a1c578cbb2c4154784e
ANUBHAV RITOLIA <anubhavritolia@gmail.com>
2023-11-01 17:15:02 +0530

    Update allChildIncidents.js
    Updated Code

--- GlideRecord/List of Child Incident of All Incidents/allChildIncidents.js
+++ GlideRecord/List of Child Incident of All Incidents/allChildIncidents.js
@@ -1,6 +1,7 @@
 var inc = new GlideRecord('incident');
+inc.addEncodedQuery(query); // You can add your filter query to check it for particular list of Incidents like Department wise, Last created on 30 days, etc.
+inc.setLimit(x); // If you want you can limit the records to some extend to avoid performance issues.
 inc.query();
-
 
 while(inc.next())
 {
