d39c0aa427c664a752e5f5e2280cf77b741fc427
kmxo <camacho.technicaltrainer@gmail.com>
2024-10-01 17:42:05 -0300

    hide

---
+++ Server Side/HideRelatedList/script.js
@@ -0,0 +1,10 @@
+function onLoad() {
+   
+    var createdBy = g_form.getValue('sys_created_by');
+    var loggedUser = g_user.userName;
+    
+    if (createdBy == loggedUser) {
+       g_form.hideRelatedList('put your related list name here');
+    }
+
+ } 
