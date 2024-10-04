00e77f3c9c14ef3fa6291739781143bba95da995
vermaamit1664 <vermaamit1664@gmail.com>
2024-10-04 18:43:42 +0530

    Update get-incident-count-by-priority.js
    Get the Count of Incidents grouped by Incident Priority

--- GlideAggregate/Get Incident Count by Priority/get-incident-count-by-priority.js
+++ GlideAggregate/Get Incident Count by Priority/get-incident-count-by-priority.js
@@ -1,1 +1,7 @@
-
+// Get the count of Incidents grouped by Priority
+var incidentGr = new GlideAggregate('incident');
+incidentGr.addAggregate('COUNT', 'priority');
+incidentGr.query();
+while (incidentGr.next()) {
+    gs.print(incidentGr.priority.getDisplayValue() + '=' + incidentGr.getAggregate('COUNT', 'priority'));
+}
