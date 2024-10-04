b3403a482a6f4b294b818f415c0299d0cdce66ff
Naveen Rawat <naveenraw25@gmail.com>
2024-10-04 22:56:20 +0530

    Added isValidJSON function to verify if the string is valid JSON and log the result

--- Background Scripts/Check String is Valid JSON/checkStringisValidJson.js
+++ Background Scripts/Check String is Valid JSON/checkStringisValidJson.js
@@ -1,1 +1,22 @@
+// Function to check if a string is a valid JSON
+function isValidJSON(str) {
+    try {
+        // Try to parse the string as JSON
+        JSON.parse(str);
+    } catch (e) {
+        // If an error occurs, the string is not valid JSON
+        return false;
+    }
+    // If no error occurs, the string is valid JSON
+    return true;                    
+}
 
+// Example JSON string
+const str = '{ "firstName":"John" , "lastName": "Doe"}'; 
+
+// Check if the string is valid JSON and log the result
+if (isValidJSON(str)) {
+  console.log('String is valid JSON');
+} else {
+  console.log('String is not valid JSON');
+}
