e938fe0b46f0d6ddf36597db9d1d95a96d9a8f0b
Abhrajyoti Kanrar <abhrajyotikanrar1996@gmail.com>
2024-10-05 03:19:42 +0800

    Update Reject RITM.js

--- Fix scripts/Reject RITM via fix script/Reject RITM.js
+++ Fix scripts/Reject RITM via fix script/Reject RITM.js
@@ -3,13 +3,13 @@ item.addQuery("sys_id","31cd7552db252200a6a2b31be0b8f55c"); // sys_id of the RIT
 item.query();
 if(item.next()){
   var approval = new GlideRecord("sysapproval_approver");
-  Approval.addQuery("document_id",item.getUniqueValue(); // Get the sys_Id of document Id field on approval table
-  Approval.query();
-  if(Approval.next()){
-    Approval.state="Rejected";
+  approval.addQuery("document_id",item.getUniqueValue()); // Get the sys_Id of document Id field on approval table
+  approval.query();
+  if(approval.next()){
+    approval.state="Rejected";
     item.apprpval="Rejected";
   }
   item.setWorkflow(false);
   item.update();
-  Approval.update(); // Update the record on approval table.
+  approval.update(); // Update the record on approval table.
 }
