a56fd1fb5cea7887e8a6bca53898febc6c750ced
Mandeep Karan <mandeep.karan0@gmail.com>
2024-10-06 15:59:07 +0530

    Create update_notes_tag_addition.js
    Business rule to add work notes on record if notes for tag entries are allowed.
    It is very hard to track who added the tag to the record and when, this will help to manage to understand who has added what tags on record.

---
+++ Business Rules/Add notes for tag addition or removal/update_notes_tag_addition.js
@@ -0,0 +1,38 @@
+/**********************BR COnfig Start***************************/
+/*
+When: async
+order: 2000
+insert: true
+Filter: labelISNOTEMPTY^label.nameISNOTEMPTY^EQ
+*/
+/**********************BR COnfig End***************************/
+
+(function executeRule(current, previous /*null when async*/ ) {
+    // Check if logging for tag additions is enabled
+    if (gs.getProperty('custom.tag_entries.log_addition').toString() == "true") {
+        var current_table = current.getValue('table'); // Get the current table name
+        var allowed_tables = gs.getProperty('custom.tag_entries.tables'); // Get allowed tables for addition of notes from tag entries from properties
+        allowed_tables = allowed_tables.split(','); // Split into an array
+
+        // Verify if the current table is in the allowed list
+        if (allowed_tables.indexOf(current_table) > -1) {
+            var gr_task = new GlideRecord(current_table); // Instantiate a GlideRecord for the current table
+            try {
+                // Query the record using sys_id stored in table_key
+                gr_task.addQuery("sys_id", current.table_key.getValue());
+                gr_task.query(); // Execute the query
+                
+                // If record found, update work_notes if the field is valid
+                if (gr_task.next()) {
+                    if (gr_task.isValidField('work_notes')) {
+                        gr_task.work_notes = "Tag added: " + current.getDisplayValue('label'); // Append tag info
+                        gr_task.update(); // Save changes
+                    }
+                }
+            } catch (e) {
+                // Log any exceptions encountered during execution
+                gs.log("Exception occurred in the business rule Updating record when tagged" + e.message);
+            }
+        }
+    }
+})(current, previous);
