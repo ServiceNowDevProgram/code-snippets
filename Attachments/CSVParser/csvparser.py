9b651dd0cf258c99e5ebb0cc9555c0c368015f5d
Ashwathama007 <111859913+vivaan01@users.noreply.github.com>
2024-10-10 04:57:38 +0530

    Create csvparser.py

---
+++ Attachments/CSVParser/csvparser.py
@@ -0,0 +1,35 @@
+# importing csv module
+import csv
+
+# csv file name
+filename = "aapl.csv"
+
+# initializing the titles and rows list
+fields = []
+rows = []
+
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+    # extracting field names through first row
+    fields = next(csvreader)
+
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+
+    # get total number of rows
+    print("Total no. of rows: %d" % (csvreader.line_num))
+
+# printing the field names
+print('Field names are:' + ', '.join(field for field in fields))
+
+# printing first 5 rows
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+    # parsing each column of a row
+    for col in row:
+        print("%10s" % col, end=" "),
+    print('\n')
