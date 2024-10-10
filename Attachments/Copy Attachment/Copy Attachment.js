2a5ca75a1773e84998e63e845ca9aea590252ae9
spnegi <150329891+spnegi@users.noreply.github.com>
2024-10-10 13:33:46 +0530

    Delete Attachments/Copy Attachment directory

--- Attachments/Copy Attachment/Copy Attachment.js
+++
@@ -1,5 +0,0 @@
-var att = new GlideSysAttachment();
-var hr_id = new GlideRecord('sn_hr_core_case');
-hr_id.get("1111111111111111111111111111111);//sys_id of record
-att.copy("incident",current.sys_id,"sn_hr_core_case", hr_id.sys_id);
-
