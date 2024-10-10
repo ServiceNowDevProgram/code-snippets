8f3f4887b671712571fa7f9665992a29b0ce3d58
spnegi <150329891+spnegi@users.noreply.github.com>
2024-10-10 13:41:05 +0530

    Create Copy Attachments via Script.js

---
+++ Attachments/Copy Attachments/Copy Attachments via Script.js
@@ -0,0 +1,4 @@
+var att = new GlideSysAttachment();
+var hr_id = new GlideRecord('sn_hr_core_case');
+hr_id.get("1111111111111111111111111111111);//sys_id of record
+att.copy("incident",current.sys_id,"sn_hr_core_case", hr_id.sys_id);
