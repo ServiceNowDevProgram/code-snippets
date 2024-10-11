914608f30d2eb7c9874112a7af34f5ad2e4edaa1
Carlos <56931121+kmxo@users.noreply.github.com>
2024-10-11 16:47:04 -0300

    Update Create attachment

--- Attachments/Create Attachments/Create attachment via script.js
+++ Attachments/Create Attachments/Create attachment via script.js
@@ -1,14 +1,13 @@
-var attachmentSource = 'billing_case:' +current.sys_id;
-				
-		var attachmentPayload = current.data_export_to_pd; // this field contains the payload of attachment
-		var encodedPayload = gs.base64Encode(attachmentPayload);
-		
-		var rec = new GlideRecord('ecc_queue');
-		rec.initialize();
-		rec.agent = "AttachmentCreator";//Name of agent to process attachment creation
-		rec.topic = "AttachmentCreator";
-		rec.name = attachmentName; //file name
-		rec.source = attachmentSource; // source from where the file is coming
-		rec.payload = encodedPayload; //file content
-		
-		var flag = rec.insert();
+//It can be used within a Business Rule - After Insert
+(function executeRule(current, previous /*null when async*/) {
+
+	insertAttachment();
+
+})(current, previous);
+
+function insertAttachment() {
+
+	var gsa = new GlideSysAttachment(); 
+	var attachmentId = gsa.write(current, "fileName.txt", 'text/plain', "some data");
+
+}
