0a6b5896aafeecb64bf7e59dd07e4893d19b4b12
Sai Charan Koratala <sai.koratala@gmail.com>
2024-10-13 00:43:27 +0530

    Update Adding bookmark into Favorites tab.js
    instead of querying the grmember table to find the groups which the logged-in user is a part of, I have made use of the OOTB dynamic filters functionality as per wiz0floyd's comments

--- Background Scripts/Adding bookmark to Favorites tab/Adding bookmark into Favorites tab.js
+++ Background Scripts/Adding bookmark to Favorites tab/Adding bookmark into Favorites tab.js
@@ -1,14 +1,4 @@
-var memGroups = []; // empty array for getting the group sys'id rin future.
-var gr = new GlideRecord("sys_user_grmember");
-gr.addQuery("user", gs.getUserID()); //querying grmember table with the logged-in user to get user's groups.
-//gs.print("query is "+gr.getEncodedQuery());
-gr.query();
-while (gr.next()) {
-    memGroups.push(gr.group);//pushing the group's sys_id's into the array
-}
-//gs.print("groups "+memGroups.join(","));
-
-var filter = "assignment_groupIN" + memGroups.join(", ");// creating filter using the groups array
+var filter = "active=true^assignment_groupDYNAMICd6435e965f510100a9ad2572f2b47744";// using dynamic filter we are filtering the assignments groups of the logged-in user
 
 var listURL = "/incident_list.do?sysparm_query=" + encodeURIComponent(filter); //creating the url with the filter to showcase the list of tickets assigned to the groups which the user is a part of.
 
@@ -18,11 +8,12 @@ bookmark.addQuery("url", listURL);
 bookmark.query();
 if (!bookmark.next()) { //if not available then we are creating a new bookmark under favorites tab of the logged-in users
     var newBookmark = new GlideRecord("sys_ui_bookmark");
-    newBookmark.initialize();
-	newBookmark.order
-    newBookmark.user = gs.getUserID();
-    newBookmark.url = listURL;
-    newBookmark.title = "Incidents assigned to my groups";
-    newBookmark.pinned = true;
-    newBookmark.insert();
+	newBookmark.initialize();
+	newBookmark.order=9;
+	newBookmark.icon="list";
+	newBookmark.user = gs.getUserID();
+	newBookmark.url = listURL;
+	newBookmark.title = "Incidents assigned to my groups";
+    	newBookmark.pinned = true;
+    	newBookmark.insert();
 }
