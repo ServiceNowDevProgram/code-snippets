8bf5f97d83daed890bf74c53e09035542c67662c
Atul- LearnNGrow Together <atul.grover09@gmail.com>
2024-10-12 15:55:01 +0100

    Create ShortDes_Validation.js
    This script will make the short_ description mandatory.

---
+++ Client Scripts/Validate Short Description/ShortDes_Validation.js
@@ -0,0 +1,5 @@
+function onSubmit() {
+   //Type appropriate comment here, and begin script below
+   g_form.setMandatory(short_description);
+   // This is not a real code, I am just creating a video to demo.
+}
