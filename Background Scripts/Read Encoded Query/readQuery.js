1e4bd722dfc1aa8abb628e9f45124bccf691bc20
priyasnexpert <priyasnexpert@gmail.com>
2024-10-13 17:46:46 +0530

    readQuery.js
    This code get the encoded query in readable format.

--- Background Scripts/Read Encoded Query/readQuery.js
+++ Background Scripts/Read Encoded Query/readQuery.js
@@ -1,5 +1,5 @@
 // This code get the encoded query in readable format.
-// Copy and paste encoded query in quotes of the function API whoch you want to read.
+// Pass the table name and encoded query in the function API whoch you want to read.
 
-var grQ= new GlideQueryBreadcrumbs().getReadableQuery('');
+var grQ= new GlideQueryBreadcrumbs().getReadableQuery('table name', 'Pass the encoded query');
 gs.info("Readable Query is "+grQ);
