2dc2fd95b27b4bae060a8c44239d88b1cf0991a4
Harsh Deep Singh <58880397+Harshuu870@users.noreply.github.com>
2024-10-15 13:09:05 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
 You can attach a report as PDF in email notification. To do that just add the following line to your body.
 
-$freport:reportID:YOUR_REPORT_SYS_ID)
-
+${report:reportID: YOUR_REPORT_SYS_ID }
 Replace YOUR_REPORT_SYS_ID with sys_ id of your report.
