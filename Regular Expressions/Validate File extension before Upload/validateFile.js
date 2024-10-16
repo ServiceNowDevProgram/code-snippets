6e3916d30d143fb2271845e08a4576cb5e1d8b43
HONG QI XING <qixing1994@hotmail.com>
2024-10-17 00:59:57 +0800

    Create validateFile.js

---
+++ Regular Expressions/Validate File extension before Upload/validateFile.js
@@ -0,0 +1,9 @@
+const fileAllowed = ([a-zA-Z0-9\s_\\.\-:])+(\.doc|\.docx|\.pdf)$;
+
+const file = "ABC.doc";
+
+if (fileAllowed.test(file)) {
+  console.log("Valid file for upload");
+} else {
+  console.log("Invalid file upload");
+}
