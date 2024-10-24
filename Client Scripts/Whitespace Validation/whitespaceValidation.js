c5bc22f30fc677c604989ccd1567f2566a5c8ea2
Prashant Kumar <prashantmrshine@gmail.com>
2024-10-24 10:35:31 +0530

    Update whitespaceValidation.js

--- Client Scripts/Whitespace Validation/whitespaceValidation.js
+++ Client Scripts/Whitespace Validation/whitespaceValidation.js
@@ -1,9 +1,9 @@
 // Client Script to Validate Whitespaces
-var a = g_form.getValue('field_name'); // Get value from the field
-	var b = a.length;
-	for (var i=0; i<b; ++i) {
-    if (a.charAt(i) === ' ') {
-        alert('Field Name cannot have spaces!'); // Alert if field contains whitespace
-	      g_form.setValue('field_name',''); // Empty field for any whitespaces
-    }
+var reg = /\s/;
+var value = g_form.getValue('field_name');
+var k = reg.test(value);
+
+if (k == true) {
+    alert('Field Name cannot have spaces!'); // Alert if field contains whitespace
+    g_form.setValue('field_name', ''); // Empty field for any whitespaces
 }
