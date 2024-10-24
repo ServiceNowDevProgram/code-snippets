c1de6767c672e98ef4ea45a6625545ffd22028b6
Charanjeet <35090930+Charanjet@users.noreply.github.com>
2024-10-24 21:28:59 +0530

    Update script.js
    multiple table query removed

--- Background Scripts/listUsersHavingSpecificRole/script.js
+++ Background Scripts/listUsersHavingSpecificRole/script.js
@@ -1,14 +1,11 @@
 function getUsersBasedOnrole(role){
-	var userGR = new GlideRecord('sys_user');
-	var roleGR = new GlideRecord('sys_user_has_role');
 	var itilUsers = [];
-	roleGR.addQuery('role.name', role);
-	roleGR.query();
-	
-	while (roleGR.next()) {
-		userGR.get(roleGR.user);
+	var userGR = new GlideRecord('sys_user');
+	userGR.addEncodedQuery('roles.name=itil');
+	userGR.query();
+	while (userGR.next()) {
 		itilUsers.push(userGR.user_name.toString());
 	}
-  gs.info('Users with '+ role +' role: ' + itilUsers.join(', '));
-  }
+	gs.info('Users with '+ role +' role: ' + itilUsers.join(', '));
+}
 getUsersBasedOnrole('itil');
