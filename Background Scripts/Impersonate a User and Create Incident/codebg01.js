8f8ea1e8cd680d52e843abdb8a35071e5e115c99
ShakthiSn <shakthi.snow23@gmail.com>
2024-10-27 20:17:03 +0530

    Update codebg01.js

--- Background Scripts/Impersonate a User and Create Incident/codebg01.js
+++ Background Scripts/Impersonate a User and Create Incident/codebg01.js
@@ -1,1 +1,8 @@
-
+var user=new GlideUser();
+user.setUserByID('itil');
+gs.print("Impersonated User : "+user.getDisplayName());
+var gr=new GlideRecord("incident");
+gr.initialize();
+gr.short_description="Incident Created by Impersonated User";
+gr.insert();
+gs.print("Created Incident by "+ user.getDisplayName());
