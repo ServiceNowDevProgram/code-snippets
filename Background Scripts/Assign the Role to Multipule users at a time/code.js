59ea9af16e56f9553373cd614b4a0d015e451103
ShakthiSn <shakthi.snow23@gmail.com>
2024-10-28 19:04:06 +0530

    Update code.js

--- Background Scripts/Assign the Role to Multipule users at a time/code.js
+++ Background Scripts/Assign the Role to Multipule users at a time/code.js
@@ -1,1 +1,14 @@
-var ne=new GlideRecord('incident');
+var gr=new GlideRecord("sys_user");
+gr.addEncodedQuery('departmentISEMPTY'); //User who have the Department is empty 
+//gr.setLimit(5);
+gr.query();
+while(gr.next())
+{
+	var urole=new GlideRecord('sys_user_has_role');
+	urole.initialize();
+	urole.user=gr.sys_id;
+	urole.role='b3dd3ccec32203003e76741e81d3ae95'; //Role SysID
+	urole.insert();
+	gs.print("Added role to user : "+gr.name);
+
+}
