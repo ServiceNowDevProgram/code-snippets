15c991bf3b4084cbf1bdd890a6a1185c0b0e4133
rohi-v <91993658+rohi-v@users.noreply.github.com>
2024-10-29 00:10:13 +0530

    Create ParseUrl.js

---
+++ UI Actions/Open Record producer from Form Button In Configurable workspace/ParseUrl.js
@@ -0,0 +1,26 @@
+function onLoad() {
+
+  
+	g_form.setValue('task',parseURL('sysparm_parent_sys_id'));
+	g_form.setValue('description',parseURL('sysparm_description));
+	
+	function parseURL(paramName) {
+		
+		var url = decodeURIComponent(top.location.href); //Get the URL and decode it
+		var workspaceParams = url.split('extra-params/')[1]; //Split off the url on Extra params
+		var allParams = workspaceParams.split('/'); //The params are split on slashes '/'
+		
+		//Search for the parameter requested
+		for (var i=0; i< allParams.length; i++) {
+			if(allParams[i] == paramName) {
+				return allParams[i+1];
+			}
+		}
+	}
+}
+/*
+
+pass the parameter name which was used in the ui action to parse the value here.
+
+Example - parseURL('sysparm_description)
+*/
