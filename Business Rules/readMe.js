435913536ca89989981f118eb830c22ae3c23766
Soumyadeep Dutta <78016846+Soumyadeep10@users.noreply.github.com>
2024-10-30 16:38:04 +0530

    Update and rename readMe.js to readme.md

--- Business Rules/readMe.js
+++
@@ -1,6 +0,0 @@
-In this before insert/update Business Rule
-we are fetching a reference field value from higher-level parents in hierarchy 
-when there is a field containing the parent record in the children and 
-our use-case reference field is present in all the tables in hierarchy
-
-I am checking the empty value of the use-case reference field and dot walking to parents and fetching the value from them if it exists
