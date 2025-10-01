2f515d14f3fb93c565d719c5f9f9c48d92d3beb3
Naveen Kumar <103413520+naveensnow@users.noreply.github.com>
2025-10-01 13:19:49 +0530

    Create scriptinclude.js

---
+++ Client-Side Components/UI Pages/Edit Last WorkNotes/scriptinclude.js
@@ -0,0 +1,53 @@
+var UpdateINCworkNotes = Class.create();
+UpdateINCworkNotes.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+
+    getIncLastWorknotes: function() {
+        var id = this.getParameter('sysparm_id');
+        var table = 'incident';
+        var list = new GlideRecord("sys_journal_field");
+        list.addEncodedQuery("element_id=" + id + "^element=comments^ORelement=work_notes");
+
+        list.orderByDesc('sys_created_on');
+        list.setLimit(1);
+        list.query();
+        list.next();
+        return list.value.toString()
+    },
+
+
+    updateCommentsLatest: function() {
+        var id = this.getParameter('sysparm_id');
+        var newcomment = this.getParameter('sysparm_newcomment');
+        var table = 'incident';
+        var list = new GlideRecord("sys_journal_field");
+        list.addEncodedQuery("element_id=" + id + "^element=comments^ORelement=work_notes");
+        list.orderByDesc('sys_created_on');
+        list.setLimit(1);
+        list.query();
+        list.next();
+        list.value = newcomment;
+        list.update();
+
+        var list1 = new GlideRecord("sys_audit");
+        list1.addEncodedQuery("documentkey=" + id + "^fieldname=comments^ORfieldname=work_notes");
+        list1.setLimit(1);
+        list1.orderByDesc('sys_created_on');
+        list1.query();
+        if (list1.next()) {
+            list1.newvalue = newcomment
+            list1.oldvalue = '';
+            list1.update();
+        }
+
+        var list3 = new GlideRecord("sys_history_set");
+        list3.addEncodedQuery("id=" + id);
+        list3.setLimit(1);
+        list3.query();
+        if (list3.next()) {
+            list3.deleteRecord();
+        }
+       window.location.reload();
+    },
+
+    type: 'UpdateINCworkNotes'
+});
