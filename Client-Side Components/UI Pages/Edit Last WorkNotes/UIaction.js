05cad836a98c605adc76b930a44c2a7123a21cf1
Naveen Kumar <103413520+naveensnow@users.noreply.github.com>
2025-10-01 15:55:02 +0530

    Update UIaction.js
    spelling mistake in title are corrected

--- Client-Side Components/UI Pages/Edit Last WorkNotes/UIaction.js
+++ Client-Side Components/UI Pages/Edit Last WorkNotes/UIaction.js
@@ -1,6 +1,6 @@
 function openEditLastCommentModal() {
     var dialog = new GlideModal("edit_worknotes_comments_inc");
-    dialog.setTitle('Edit Last WorkNotes/Addtional Comments');
+    dialog.setTitle('Edit Last WorkNotes/Additional Comments');
     dialog.setPreference('incid', g_form.getUniqueValue());	      	
     dialog.setWidth(550);
     dialog.render();
