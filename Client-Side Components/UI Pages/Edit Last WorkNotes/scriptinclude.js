fe96e9d33b5a5b7fc9861f6fa03e160dda8faefa
Naveen Kumar <103413520+naveensnow@users.noreply.github.com>
2025-10-01 15:45:50 +0530

    Refactored code with null checks and descriptive names
    Refactored code with null checks and descriptive names

--- Client-Side Components/UI Pages/Edit Last WorkNotes/scriptinclude.js
+++ Client-Side Components/UI Pages/Edit Last WorkNotes/scriptinclude.js
@@ -1,53 +1,74 @@
-var UpdateINCworkNotes = Class.create();
-UpdateINCworkNotes.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+var UpdateCommentsworkNotes = Class.create();
+UpdateCommentsworkNotes.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 
     getIncLastWorknotes: function() {
-        var id = this.getParameter('sysparm_id');
-       // var table = 'incident';
-        var list = new GlideRecord("sys_journal_field");
-        list.addEncodedQuery("element_id=" + id + "^element=comments^ORelement=work_notes");
+        var recordId = this.getParameter('sysparm_id');
+        // Check if the record ID is provided and is not null or undefined
+        if (!recordId) {
+            gs.error("UpdateINCworkNotes.getIncLastWorknotes: No record ID (sysparm_id) provided.");
+            return '';
+        }
 
-        list.orderByDesc('sys_created_on');
-        list.setLimit(1);
-        list.query();
-        list.next();
-        return list.value.toString()
+        var grJournal = new GlideRecord("sys_journal_field");
+        grJournal.addEncodedQuery("element_id=" + recordId + "^element=comments^ORelement=work_notes");
+        grJournal.orderByDesc('sys_created_on');
+        grJournal.setLimit(1);
+        grJournal.query();
+
+        if (grJournal.next()) {
+            return grJournal.getValue('value');
+        }
+
+        return '';
     },
-
 
     updateCommentsLatest: function() {
-        var id = this.getParameter('sysparm_id');
-        var newcomment = this.getParameter('sysparm_newcomment');
-     //   var table = 'incident';
-        var list = new GlideRecord("sys_journal_field");
-        list.addEncodedQuery("element_id=" + id + "^element=comments^ORelement=work_notes");
-        list.orderByDesc('sys_created_on');
-        list.setLimit(1);
-        list.query();
-        list.next();
-        list.value = newcomment;
-        list.update();
-
-        var list1 = new GlideRecord("sys_audit");
-        list1.addEncodedQuery("documentkey=" + id + "^fieldname=comments^ORfieldname=work_notes");
-        list1.setLimit(1);
-        list1.orderByDesc('sys_created_on');
-        list1.query();
-        if (list1.next()) {
-            list1.newvalue = newcomment
-            list1.oldvalue = '';
-            list1.update();
+        var recordId = this.getParameter('sysparm_id');
+        var newComment = this.getParameter('sysparm_newcomment');
+        // Validate input parameters
+        if (!recordId || !newComment) {
+            gs.error("UpdateINCworkNotes.updateCommentsLatest: Missing required parameters (sysparm_id or sysparm_newcomment).");
+            return "failure: Missing parameters.";
         }
 
-        var list3 = new GlideRecord("sys_history_set");
-        list3.addEncodedQuery("id=" + id);
-        list3.setLimit(1);
-        list3.query();
-        if (list3.next()) {
-            list3.deleteRecord();
+        // Update the latest journal entry for the incident
+        var grJournal = new GlideRecord("sys_journal_field");
+        grJournal.addEncodedQuery("element_id=" + recordId + "^element=comments^ORelement=work_notes");
+        grJournal.orderByDesc('sys_created_on');
+        grJournal.setLimit(1);
+        grJournal.query();
+
+        if (grJournal.next()) {
+            grJournal.setValue('value', newComment);
+            grJournal.update();
+        } else {
+            // Log if no journal field was found to update
+            gs.warn("UpdateINCworkNotes.updateCommentsLatest: No latest journal entry found for record ID: " + recordId);
         }
-       window.location.reload();
+
+        var grAudit = new GlideRecord("sys_audit");
+        grAudit.addEncodedQuery("documentkey=" + recordId + "^fieldname=comments^ORfieldname=work_notes");
+        grAudit.orderByDesc('sys_created_on');
+        grAudit.setLimit(1);
+        grAudit.query();
+
+        if (grAudit.next()) {
+            grAudit.setValue('newvalue', newComment);
+            grAudit.setValue('oldvalue', '');
+            grAudit.update();
+        } else {
+            gs.warn("UpdateINCworkNotes.updateCommentsLatest: No latest audit entry found for record ID: " + recordId);
+        }
+
+        var grHistorySet = new GlideRecord("sys_history_set");
+        grHistorySet.addQuery("id", recordId);
+        grHistorySet.setLimit(1);
+        grHistorySet.query();
+
+        if (grHistorySet.next()) {
+            grHistorySet.deleteRecord();
+        }
     },
 
-    type: 'UpdateINCworkNotes'
+    type: 'UpdateCommentsworkNotes'
 });
