f8481a29e219385fbd2271db272bcdb684eb4141
GHSayak25 <59216790+GHSayak25@users.noreply.github.com>
2025-10-02 16:40:45 +0530

    Update Do Task on weekdays.js

--- Server-Side Components/Background Scripts/Execute Logic on Weekdays/Do Task on weekdays.js
+++ Server-Side Components/Background Scripts/Execute Logic on Weekdays/Do Task on weekdays.js
@@ -1,4 +1,4 @@
-//Code that checks if today is Saturday or Sunday (i.e. Weekend) and if it turns out that today is weekend, do not execute the further logic written.
+//Code that checks if today is not Saturday or Sunday (i.e. Weekend) and if it turns out that today is not weekend, then execute the further logic written.
 
 var today = new GlideDateTime();
 var day = today.getDayOfWeek(); //returns the day of week as number, Monday = 1, Tuesday = 2, ... Saturday = 6, Sunday = 7
