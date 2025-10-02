22b4449943b29d5166332523604a45dc6ef647ab
Abhishek Thakur <146176327+at8807602@users.noreply.github.com>
2025-10-02 11:18:43 +0530

    Delete Server-Side Components/Background Scripts/Change Approver directory

--- Server-Side Components/Background Scripts/Change Approver/BgScript.js
+++
@@ -1,9 +0,0 @@
-
-var ab=new GlideRecord('sysapproval_approver');
-ab.addQuery('sysapproval','<Approval_record_sys_id>');
-ab.query();
-if(ab.next())
-{
-ab.approver.setDisplayValue('<User_Display_Name>')';
-ab.update();
-}
