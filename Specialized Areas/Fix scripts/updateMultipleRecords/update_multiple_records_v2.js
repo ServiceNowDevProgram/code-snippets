1cf3eb9e14abac4ed71c7e1e841b61169016ce18
Mexiprince <34014440+Mexiprince@users.noreply.github.com>
2025-10-02 18:45:25 -0500

    Update update_multiple_records_v2.js

--- Specialized Areas/Fix scripts/updateMultipleRecords/update_multiple_records_v2.js
+++ Specialized Areas/Fix scripts/updateMultipleRecords/update_multiple_records_v2.js
@@ -3,7 +3,6 @@ var GrQry = ""; //Query of the affected records.
 var grTableName = new GlideRecord('table_name');
 grTableName.addEncodedQuery(GrQry);
 grTableName.query();
-gs.addInfoMessage(grTableName.getRowCount())// comment out after validating row count
   
 while (grTableName.next()) {
 grTableName.setValue("field", "value"); // Replace 'field' and 'value'
@@ -11,3 +10,4 @@ grTableName.autoSysFields(false); // Prevents updating system fields like 'updat
 grTableName.setWorkflow(false); // Prevents triggering workflows
 grTableName.update(); 
     }
+gs.print("Records updated successfully");
