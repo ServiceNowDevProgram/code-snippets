88401f2de5efb4aed80275a419198c2579597aba
SaiSankar1309 <saisankar1309@gmail.com>
2025-10-03 18:19:18 +0200

    created a separate file
    created a separate file for onChange client script

--- Server-Side Components/Script Includes/Business Service Fetch When CI Changes/scriptinclude.js
+++ Server-Side Components/Script Includes/Business Service Fetch When CI Changes/scriptinclude.js
@@ -41,27 +41,3 @@ getServiceDetails.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 });
 
 
-/**onChange Client Script on Change_Request form when CI changes */
-
-function onchange(control, oldValue, newValue, isLoading) {
-    if (isLoading || newValue == '') {
-        return;
-    }
-
-    var ga = new GlideAjax('getServiceDetails');
-    ga.addParam('sysparm_name', 'getService');
-    ga.addParam('sysparm_ci_sys_id', newValue);
-    ga.getXML(callScriptInclude);
-
-    function callScriptInclude(response) {
-        var answer = response.responseXML.documentElement.getAttribute("answer");
-
-        if (answer) {
-            // Set the Business Service field with the service name returned
-            g_form.setValue('business_service', answer);
-        } else {
-            // Optional: show a message if no service is found
-            g_form.showFieldMsg('business_service', 'No linked Business Service found for this CI.', 'info');
-        }
-    }
-}
