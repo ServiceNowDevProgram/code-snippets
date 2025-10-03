08a76ad785e42d7b2596e5f301a0b86546aa2e33
Viraj Hudlikar <31214018+vhudlikar@users.noreply.github.com>
2025-10-04 00:56:53 +0530

    Update IndianGSTINValidator.js

--- Specialized Areas/Regular Expressions/Indian GSTIN Validator/IndianGSTINValidator.js
+++ Specialized Areas/Regular Expressions/Indian GSTIN Validator/IndianGSTINValidator.js
@@ -1,37 +1,48 @@
-// Alphanumeric regex pattern
-var alphanumericPattern = /^[a-zA-Z0-9]*$/;
 
-// Function to validate if a string is alphanumeric
-function isAlphanumeric(str) {
-    return alphanumericPattern.test(str);
-}
+function isValidGSTNo(str) {
+    // Regex to check valid GST CODE
+    var regex = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/;
 
-// Example usage
-var examples = [
-    "abc123",    // Valid
-    "ABC",       // Valid
-    "123",       // Valid
-    "abc123!",   // Invalid (contains '!')
-    "hello world", // Invalid (contains space)
-    "123-456",   // Invalid (contains '-')
-];
+    // GST CODE is empty return false
+    if (str == null) {
+        return "false";
+    }
 
-// Test the examples using a for loop
-for (var i = 0; i < examples.length; i++) {
-    var example = examples[i];
-    if (isAlphanumeric(example)) {
-        gs.print(example+" is a valid alphanumeric string.");
-    } else {
-        gs.print(example+ " is NOT a valid alphanumeric string.");
+    // Return true if the GST_CODE matched the ReGex
+    if (regex.test(str) == true) {
+        return "true";
+    }
+    else {
+        return "false";
     }
 }
 
+// Test Case 1:
+var str1 = "33AAACH1645P2ZH";
+gs.print(str1 +' isValidGSTNo '+isValidGSTNo(str1));
+
+// Test Case 2:
+var str2 = "06BZAF67";
+gs.print(str2 +' isValidGSTNo '+isValidGSTNo(str2));
+
+// Test Case 3:
+var str3 = "AZBZAHM6385P6Z2";
+gs.print(str3 +' isValidGSTNo '+isValidGSTNo(str3));
+
+// Test Case 4:
+var str4 = "36AAICG1508J1ZN";
+gs.print(str4 +' isValidGSTNo '+isValidGSTNo(str4));
+
+// Test Case 5:
+var str5 = "27AAUCS0795B1Z0";
+gs.print(str5 +' isValidGSTNo '+isValidGSTNo(str5));
+
+
 /*
-when you run this code, it will output:
-*** Script: abc123 is a valid alphanumeric string.
-*** Script: ABC is a valid alphanumeric string.
-*** Script: 123 is a valid alphanumeric string.
-*** Script: abc123! is NOT a valid alphanumeric string.
-*** Script: hello world is NOT a valid alphanumeric string.
-*** Script: 123-456 is NOT a valid alphanumeric string.
+When you run this code, it will output:
+*** Script: 33AAACH1645P2ZH isValidGSTNo true
+*** Script: 06BZAF67 isValidGSTNo false
+*** Script: AZBZAHM6385P6Z2 isValidGSTNo false
+*** Script: 36AAICG1508J1ZN isValidGSTNo true
+*** Script: 27AAUCS0795B1Z0 isValidGSTNo true
 */
