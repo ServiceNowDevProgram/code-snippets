094f849092bb362c8f9f6dc1a3c3ff192f2aca88
Naveen Kumar <103413520+naveensnow@users.noreply.github.com>
2025-10-04 14:20:04 +0530

    Update ui_page_client.js
    making the values mandatory

--- Client-Side Components/UI Pages/Resolve Incident UI Page/ui_page_client.js
+++ Client-Side Components/UI Pages/Resolve Incident UI Page/ui_page_client.js
@@ -1,8 +1,16 @@
 // Below code will be used in client script of UI page as mentioned in README.md file
 
 function ResolveIncidentOnsubmit(sysId) {     //This function is called in UI page HTML section When user clicks the Submit button
-      var rejectionReason = document.getElementById('resolution_reason').value;
-      var resolutionCode = document.getElementById('resolution_code').value;
+      var rejectionReason = document.getElementById('resolution_reason').value.trim();
+      var resolutionCode = document.getElementById('resolution_code').value.trim();
+      if (!rejectionReason || rejectionReason === ' ') {
+          alert('Resolution Notes is a mandatory field.');
+          return false;
+      }
+      if (resolutionCode == 'None') {
+          alert('Resolution Code is a mandatory field.');
+          return false;
+      }
       var ga = new GlideAjax('ResolutionProcessor');
       ga.addParam('sysparm_name', 'updateRecord');
       ga.addParam('sysparm_record_id', sysId);
