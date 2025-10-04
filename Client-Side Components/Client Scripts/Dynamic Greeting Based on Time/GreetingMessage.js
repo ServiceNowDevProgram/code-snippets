b3db1e2b33a273a23edd4b3716c7b8e82bef1ced
Ravi Gaurav <ravi.gaurav@cgi.com>
2025-10-04 23:50:27 +0530

    Delete Client-Side Components/Client Scripts/Dynamic Greeting Based on Time/GreetingMessage.js

--- Client-Side Components/Client Scripts/Dynamic Greeting Based on Time/GreetingMessage.js
+++
@@ -1,7 +0,0 @@
-
-// Type: onLoad | Table: incident
-function onLoad() {
-  var hr = new Date().getHours();
-  var msg = (hr < 12) ? "Good Morning!" : (hr < 18) ? "Good Afternoon!" : "Good Evening!";
-  g_form.addInfoMessage(msg + " Please provide the details of your issue.");
-}
