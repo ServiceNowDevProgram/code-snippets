3db995400e1237a1ceaf300a66ad6bf44c534676
Jaspal <30924269+Jaspalsinghchot@users.noreply.github.com>
2025-10-04 13:07:34 +0400

    Delete Background Scripts/Get Duplicate directory

--- Background Scripts/Get Duplicate/script.js
+++
@@ -1,8 +0,0 @@
-var dpchk = new GlideAggregate('task');               
-dpchk.groupBy('number');                             
-dpchk.addHaving('COUNT', '>', 1);
-dpchk.query();
-while(dpchk.next())	
-{
-		gs.print(dpchk.number);                               
-}
