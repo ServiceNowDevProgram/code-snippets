409fd7d4af4ff6713f69c1bd1d4c36013a96ec7a
SIVAMURUGANANDAM J <67639018+sivamuruganandam-coder@users.noreply.github.com>
2025-10-05 15:02:11 +0530

    Delete Client-Side Components/UI Actions/Generate PDF directory

--- Client-Side Components/UI Actions/Generate PDF/Script.js
+++
@@ -1,9 +0,0 @@
-// UI Action: Generate PDF
-// Table: Incident 
-// Condition: current.state=='6' //Resolved
-// Client true
-// sysparm_view = pdf_export 
-function onClick(g_form) {
-    var url = '/'+g_form.getTableName() + '.do?sys_id=' + g_form.getUniqueValue() + '&PDF&sysparm_view=pdf_export';
-    top.window.open(url, '_blank');
-}
