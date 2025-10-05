c0c1d1e01c2bbaec8ecb4ac2276644b59b3b0680
SIVAMURUGANANDAM J <67639018+sivamuruganandam-coder@users.noreply.github.com>
2025-10-05 13:47:03 +0530

    Update Script.js

--- Client-Side Components/UI Actions/Generate PDF/Script.js
+++ Client-Side Components/UI Actions/Generate PDF/Script.js
@@ -1,10 +1,9 @@
 // UI Action: Generate PDF
 // Table: Incident 
 // Condition: current.state=='6' //Resolved
-// Onclick: generatePDF();
+// Client true
 // sysparm_view = pdf_export 
-function generatePDF() {
-
+function onClick(g_form) {
     var url = '/'+g_form.getTableName() + '.do?sys_id=' + g_form.getUniqueValue() + '&PDF&sysparm_view=pdf_export';
     top.window.open(url, '_blank');
 }
