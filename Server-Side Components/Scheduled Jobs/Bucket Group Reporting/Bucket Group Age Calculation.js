973d16118701a4bb0b386ec157e4f7f1ac33b8e2
spnegi <150329891+spnegi@users.noreply.github.com>
2025-10-07 01:14:03 +0530

    Update Bucket Group Age Calculation.js
    adding the line 2 state values and defining variables and removing the log statements

--- Server-Side Components/Scheduled Jobs/Bucket Group Reporting/Bucket Group Age Calculation.js
+++ Server-Side Components/Scheduled Jobs/Bucket Group Reporting/Bucket Group Age Calculation.js
@@ -1,13 +1,12 @@
 var rec = new GlideRecord('case'); // any table which you want to use
-    rec.addEncodedQuery('stateNOT IN60,40, 20'); // filtering out all the closed/cancelled cases
+    rec.addEncodedQuery('stateNOT IN60,40, 20'); // filtering out all the closed/cancelled cases 60 is closed, 40 is cancelled, 20 is rejected
         rec.query();
     while (rec.next()) {
     var openedDate = new GlideDateTime(rec.opened_at.getDisplayValue());
     var dur = GlideDateTime.subtract(openedDate,actualDateTime );
-    //gs.info("dur"+dur.getNumericValue()) ;
-    elapsedTime =  dur.getNumericValue()/86400000 ;
-    //  gs.info ("elapsedTime" + elapsedTime)
+    var elapsedTime =  dur.getNumericValue()/86400000 ;
     // Check to see when the item was created
+    var aging;
     if (elapsedTime <= 2) aging = '0-2 Days';
     if (elapsedTime > 2) aging = '3-4 Days';
     if (elapsedTime > 4) aging = '5-7 Days';
@@ -19,6 +18,6 @@ var rec = new GlideRecord('case'); // any table which you want to use
  
     rec.setWorkflow(false); // Skip any Business Rules
     rec.autoSysFields(false); // Do not update system fields
-    rec.aging_category = aging;
+    rec.aging_category = aging; // updating aging category with defined buckets allocated above
     rec.update();
     }
