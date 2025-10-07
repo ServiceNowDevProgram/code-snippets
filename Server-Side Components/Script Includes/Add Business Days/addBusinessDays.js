47a15c930cc3edd25bc04a215599e09a98b5728e
HackoDev1530 <92592691+HackoDev1530@users.noreply.github.com>
2025-10-07 15:44:29 -0500

    Update addBusinessDays.js

--- Server-Side Components/Script Includes/Add Business Days/addBusinessDays.js
+++ Server-Side Components/Script Includes/Add Business Days/addBusinessDays.js
@@ -1,4 +1,6 @@
 // Takes the number of days as a parameter, and the start date
+//Name: addBusinessDays
+//Client Callable: checked
 var addBusinessDays = Class.create();
 var schedule = new GlideSchedule('090eecae0a0a0b260077e1dfa71da828');
 addBusinessDays.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
