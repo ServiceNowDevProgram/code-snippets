0fd02861dcd62c802ea9bcda5af0e20a32c5b96a
SIVAMURUGANANDAM J <67639018+sivamuruganandam-coder@users.noreply.github.com>
2025-10-07 13:56:39 +0530

    Update script.js

--- Core ServiceNow APIs/GlideDateTime/Calculate Due date using user defined schedules/script.js
+++ Core ServiceNow APIs/GlideDateTime/Calculate Due date using user defined schedules/script.js
@@ -3,23 +3,17 @@ CaclculateDueDate.prototype = {
     initialize: function() {},
 
     calculateDueDate: function(date, days_to_add) {
-        var checkDate = new GlideDateTime(date);
-        var daysToAdd = days_to_add;
+        var business_hour_per_day = 8; // This can be stored in the system property (Value in Hours) and reused
+        var duration_script = new DurationCalculator(); // OOB Script include
+        var tz = gs.getSysTimeZone(); // Get the system timezone
 
+        duration_script.setSchedule('c798c1dfc3907e1091ea5242b40131c8', tz); // Sys id of the schedule
+        duration_script.setStartDateTime(new GlideDateTime(date));
+        var total_duration = days_to_add * (business_hour_per_day * 60 * 60); // Converting the days to seconds
+        duration_script.calcDuration(total_duration);
 
-        while (daysToAdd > 0) {
-            var sched = new GlideSchedule('c798c1dfc3907e1091ea5242b40131c8'); // Schedule record SYS_ID. Currently 9-5 Weekdays and Indian Public Holiday(excluded) schedule has been used
-            checkDate.addDaysLocalTime(1);
-            if (sched.isInSchedule(checkDate)) {
-                daysToAdd--;
-
-            } else {
-                continue;
-
-            }
-        }
-
-        return checkDate.getValue();
+        var calculated_due_date = duration_script.getEndDateTime();
+        return calculated_due_date.getDisplayValue();
     },
 
     type: 'CaclculateDueDate'
