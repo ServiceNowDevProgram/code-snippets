6ca7f20ad40e9cd470663396a74cd820c1ffb5d8
Deepak Negi <120473057+dvn-lazywinner@users.noreply.github.com>
2025-10-07 18:53:55 +0530

    Update cc all group members.js
    All active users from the group

--- Integration/Mail Scripts/cc all group members/cc all group members.js
+++ Integration/Mail Scripts/cc all group members/cc all group members.js
@@ -8,6 +8,7 @@
     // Add your code here
     var grp = new GlideRecord('sys_user_grmember');  //Query to the group member table
     grp.addQuery("group", current.assignment_group);   //add a filter to query based on the current record's assignment group
+    grp.addQuery("user.active", true);   // all active users
     grp.query();
     while (grp.next()) {
        email.addAddress('cc', grp.user.email, grp.user.name); //Passing email as name and 2nd and 3rd parameter
