70dfa52bbc6843c9c48e466f2c7d2f27087264ff
Rampriya <114834474+Rampriya-S@users.noreply.github.com>
2025-10-07 17:23:44 -0400

    Delete Client-Side Components/Catalog Client Script/Auto-populate field from URL/popdatefromurl.js

--- Client-Side Components/Catalog Client Script/Auto-populate field from URL/popdatefromurl.js
+++
@@ -1,8 +0,0 @@
-//Logic to fetch the u_date field value passed in the url and setting it in the actual field.
-
-
-var fetchUrl = top.location.href; //get the URL
-
-var setDate = new URLSearchParams(gUrl).get("sysparm_u_date");  //fetch the value of date from the query parameter
-
-g_form.setValue('u_date', setDate);   //set the value to the actual field
