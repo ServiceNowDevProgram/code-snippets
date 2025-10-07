8d785e531d6da7676b20b6b7f344b13488d7669e
zstromsodt <50678210+zstromsodt@users.noreply.github.com>
2025-10-07 15:18:19 -0500

    Create timeworkedhidetimer.js

---
+++ Client-Side Components/Client Scripts/Time Worked Hide Start/timeworkedhidetimer.js
@@ -0,0 +1,7 @@
+function onLoad() {
+  
+ $j('div[id="element.incident.time_worked"]').children('.form-field-addons').hide();
+
+  //if you use other tables, just adjust the dot-walking element ie change_request.time_worked
+   
+}
