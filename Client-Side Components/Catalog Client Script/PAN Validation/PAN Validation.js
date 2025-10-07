2947cd50007bc427e3fd8dfd8b8b8c810972ff7b
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-07 21:10:31 +0530

    Update PAN Validation.js

--- Client-Side Components/Catalog Client Script/PAN Validation/PAN Validation.js
+++ Client-Side Components/Catalog Client Script/PAN Validation/PAN Validation.js
@@ -4,8 +4,9 @@ function onChange(control, oldValue, newValue, isLoading, isTemplate) {
     }
     var panNumber = g_form.getValue("pan_number"); //Get the PAN card information
     var panRegex = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/; // Regex for the PAN Card
+    var panRegexAllCases = new RegExp(panRegex, "gim"); // Regex for checking PAN card in all cases(capital, lower,mixed) and multiline text fields.
 
-    if (panRegex.test(panNumber)) {
+    if (panRegexAllCases.test(panNumber)) {
         g_form.showFieldMsg("pan_number", "Valid PAN card number.", true); //Valid PAN card enterd populates this message 
     } else {
         g_form.showErrorBox("pan_number", "InValid PAN card number.", true); //In Valid PAN card details enterd populate this message 
