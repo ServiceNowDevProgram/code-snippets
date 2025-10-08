eba85b8430fd8af2748fa597e64d92f39d612400
SIVAMURUGANANDAM J <67639018+sivamuruganandam-coder@users.noreply.github.com>
2025-10-08 15:56:49 +0530

    Update script.js

--- Server-Side Components/Transform Map Scripts/Check if the Import file is valid/script.js
+++ Server-Side Components/Transform Map Scripts/Check if the Import file is valid/script.js
@@ -4,7 +4,7 @@ Active : True
 */
 
 (function runTransformScript(source, map, log, target /*undefined onStart*/ ) {
-  
+
     var table = import_set.table_name;
     var run = new GlideAggregate(table);
     run.addQuery("sys_import_set", import_set.sys_id);
@@ -15,6 +15,17 @@ Active : True
         if (count < 1) { // Check the row count of the latest import job. If it's 0, then abort the transformation and raise a ticket (Optional)
             ignore = true;
             gs.error("File is empty. Hence aborting the transformation");
+
+			// Creating a ticket to the Help desk team
+            var incident = new GlideRecord('incident');
+            incident.initialize();
+            incident.short_description = "Import failed due to empty file";
+            incident.description = "The incoming file was empty. Please investigate.";
+            incident.assignment_group = gs.getProperty('Fallback queue'); // Store the resolver group sys id in the system property and use it here.
+            incident.impact = 3; // Modify as required
+            incident.urgency = 3; // Modify as required
+            incident.insert();
+
         }
     }
 
