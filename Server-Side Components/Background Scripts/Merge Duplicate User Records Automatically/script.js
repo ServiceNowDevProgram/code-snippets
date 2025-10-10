0ca4fe0d95ab2780e41d0d84389a3993543bcef5
DhruvBhatheja <70469942+DhruvBhatheja@users.noreply.github.com>
2025-10-10 18:53:08 +0530

    Update script.js

--- Server-Side Components/Background Scripts/Merge Duplicate User Records Automatically/script.js
+++ Server-Side Components/Background Scripts/Merge Duplicate User Records Automatically/script.js
@@ -1,43 +1,71 @@
 (function() {
-    var userEmailMap = {};
+
+    var userEmailMap = {};     
+    var userCreatedMap = {};   
+    var duplicatesFound = 0;
+
     var userGR = new GlideRecord('sys_user');
     userGR.addNotNullQuery('email');
     userGR.addQuery('active', true);
+    userGR.orderBy('email'); // Group by email 
     userGR.query();
-
-    var duplicatesFound = 0;
 
     while (userGR.next()) {
         var email = userGR.email.toLowerCase();
+        var createdOn = new GlideDateTime(userGR.sys_created_on).getNumericValue(); // Convert to timestamp
 
         if (!userEmailMap[email]) {
-            // First user with this email
+            // First user found for this email temporarily mark as master
             userEmailMap[email] = userGR.sys_id.toString();
+            userCreatedMap[email] = createdOn;
         } else {
-            // Duplicate found then  merge with master
-            duplicatesFound++;
+            // Another user with same email found 
             var masterSysId = userEmailMap[email];
+            var masterCreatedOn = userCreatedMap[email];
 
-            // Reassign related incidents
-            var incGR = new GlideRecord('incident');
-            incGR.addQuery('caller_id', userGR.sys_id);
-            incGR.query();
-            var incidentsReassigned = 0;
-            while (incGR.next()) {
-                incGR.caller_id = masterSysId;
-                incGR.update();
-                incidentsReassigned++;
+            var masterIsOlder = createdOn > masterCreatedOn ? true : false;
+
+            if (masterIsOlder) {
+                // If this user was created later, we keep the existing master
+                // earliest created record will be accepted
+                mergeDuplicateUser(userGR, masterSysId);
+            } else {
+                // If this user was created earlier, this becomes the new master
+                var oldMasterGR = new GlideRecord('sys_user');
+                if (oldMasterGR.get(masterSysId)) {
+                    mergeDuplicateUser(oldMasterGR, userGR.sys_id); // merge old master into new master
+                }
+                // Update maps
+                userEmailMap[email] = userGR.sys_id.toString();
+                userCreatedMap[email] = createdOn;
             }
 
-            // Deactivate duplicate
-            userGR.active = false;
-            userGR.u_merged_to = masterSysId; // optional custom field
-            userGR.update();
-           
-            gs.info("Merged duplicate user '" + userGR.name + "' into master. Reassigned " + incidentsReassigned + " incidents.");
+            duplicatesFound++;
         }
     }
 
     gs.info("Total duplicates merged: " + duplicatesFound);
+
+
+   
+    function mergeDuplicateUser(duplicateGR, masterSysId) {
+        // Reassign related incidents
+        var incGR = new GlideRecord('incident');
+        incGR.addQuery('caller_id', duplicateGR.sys_id);
+        incGR.query();
+        var count = 0;
+        while (incGR.next()) {
+            incGR.caller_id = masterSysId;
+            incGR.update();
+            count++;
+        }
+
+        // Deactivate  user
+        duplicateGR.active = false;
+        duplicateGR.u_merged_to = masterSysId; // optional custom field
+        duplicateGR.update();
+
+        gs.info("Merged duplicate user '" + duplicateGR.name + "' → master: " + masterSysId + ". Reassigned " + count + " incidents.");
+    }
 
 })();
