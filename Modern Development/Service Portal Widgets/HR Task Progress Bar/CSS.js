754fe20733f7143f989e3392c341deee2b79746e
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-10 21:00:59 +0530

    Create CSS.js

---
+++ Modern Development/Service Portal Widgets/HR Task Progress Bar/CSS.js
@@ -0,0 +1,15 @@
+/*Parent container using flex to adjust width automatically*/
+.parent {
+  display: flex;
+  justify-content: space-evenly;  
+  background: cornflowerblue;
+}
+/*Text (HR task) will be shown in Red color*/
+.child{
+  color:#FF0000;
+  width:100%;
+  background: lightgreen;
+}
+.child_1{
+  width:100%;
+}
