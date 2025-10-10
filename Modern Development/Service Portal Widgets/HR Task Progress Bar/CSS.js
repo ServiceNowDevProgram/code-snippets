bc01e8fef4bb004ffd3825bc88a5810875017bbf
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-10 21:08:12 +0530

    Update CSS.js

--- Modern Development/Service Portal Widgets/HR Task Progress Bar/CSS.js
+++ Modern Development/Service Portal Widgets/HR Task Progress Bar/CSS.js
@@ -4,12 +4,13 @@
   justify-content: space-evenly;  
   background: cornflowerblue;
 }
-/*Text (HR task) will be shown in Red color*/
+/*Text (HR task) will be shown in Red colo and green background*/
 .child{
   color:#FF0000;
   width:100%;
   background: lightgreen;
 }
+/*single color when task is not in WIP*/
 .child_1{
   width:100%;
 }
