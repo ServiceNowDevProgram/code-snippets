896803150f13e425d801c2b6ac7736587ae9b83b
Parth Mittal <parthmittal619@gmail.com>
2025-10-11 17:22:40 +0530

    Added Hello World client script for Hacktoberfest

---
+++ Client-Side Components/Client Scripts/Hello World Alert/HelloWorld.js
@@ -0,0 +1,10 @@
+/**
+ * Title: Hello World Client Script
+ * Description: A simple client script that shows an alert when the form loads.
+ * Author: your-github-username
+ * Category: Client Script
+ */
+
+function onLoad() {
+    alert('Hello, World! This is my first Hacktoberfest contribution 🚀');
+}
