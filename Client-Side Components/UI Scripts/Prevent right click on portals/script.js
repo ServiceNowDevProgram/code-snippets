6f23ff703c5e189a92b2c3377cca24fb00a4863f
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-12 16:51:33 +0530

    Update script.js

--- Client-Side Components/UI Scripts/Prevent right click on portals/script.js
+++ Client-Side Components/UI Scripts/Prevent right click on portals/script.js
@@ -2,6 +2,8 @@
 Prevent right-click on portal for portal pages.
 This will secure the site code, prevent users from saving images etc.
 Ideal for high security organisations.
+
+UI Type : Mobile/service portal.
 */
 (function() { // self invoking function
     document.addEventListener('contextmenu', function(event) {
