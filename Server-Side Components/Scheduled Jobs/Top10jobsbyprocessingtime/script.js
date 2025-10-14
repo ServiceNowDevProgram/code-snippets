b3c4f6aad23bb1470c64bbe0472c8d1f14bafb03
Abhishek Pandey <91930405+bird-03@users.noreply.github.com>
2025-10-14 23:05:14 +0530

    Update script.js
    output commented and updated

--- Server-Side Components/Scheduled Jobs/Top10jobsbyprocessingtime/script.js
+++ Server-Side Components/Scheduled Jobs/Top10jobsbyprocessingtime/script.js
@@ -33,3 +33,40 @@ function getResponseTimeDetails(table, query) {
     }
     return responseTime.join(',');
 }
+
+/*
+****************OUTPUT**************
+*** Script: 
+Top 10 url values from syslog_transaction
+
+
+
+********** Execution Details for the column JOB: Check Glide Service Status **********
+
+Executed total number of times : JOB: Check Glide Service Status 1
+
+Top 10 response times : 45300
+
+
+********** Execution Details for the column JOB: Regenerate CRL and Flush CRL Cache **********
+
+Executed total number of times : JOB: Regenerate CRL and Flush CRL Cache 1
+
+Top 10 response times : 1462
+
+
+********** Execution Details for the column JOB: SC - Calculate Compliance **********
+
+Executed total number of times : JOB: SC - Calculate Compliance 1
+
+Top 10 response times : 5401
+
+
+********** Execution Details for the column JOB: [ITSM Analytics] Daily Data Collection **********
+
+Executed total number of times : JOB: [ITSM Analytics] Daily Data Collection 1
+
+Top 10 response times : 16341
+
+[0:00:00.048] Total Time
+*/
