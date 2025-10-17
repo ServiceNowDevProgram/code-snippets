e247993a312a216a9c145b6b34bac11ecc952e16
HackoDev1530 <92592691+HackoDev1530@users.noreply.github.com>
2025-10-17 13:40:34 -0500

    Refactor message handling based on incident priority

--- Core ServiceNow APIs/GlideSystem/New Additional Message API/script.js
+++ Core ServiceNow APIs/GlideSystem/New Additional Message API/script.js
@@ -1,8 +1,14 @@
+//Table: Incident
+//When to Run: After update
 (function executeRule(current, previous /*null when async*/ ) {
 
-    gs.addHighMessage("This is high priority");
-    gs.addLowMessage("This is low priority");
-    gs.addModerateMessage("This is moderate priority");
-    gs.addSuccessMessage("This is a success message");
+    if (current.priority == 2)
+        gs.addHighMessage("This is a high incident, and assign it as soon as possible.");
+    if (current.state == 6)
+        gs.addLowMessage("The incident will be marked as closed automatically after 7 days.");
+    if (current.state == 6)
+        gs.addModerateMessage("Refer to the resolution information section for the resolution notes.");
+    if (current.assigned_to.changes())
+        gs.addSuccessMessage("The notification has been sent to the assignee.");
 
 })(current, previous);
