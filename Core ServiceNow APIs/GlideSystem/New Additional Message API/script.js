2846ad6a6155586a53f1e5319b7433e24ae26012
HackoDev1530 <92592691+HackoDev1530@users.noreply.github.com>
2025-10-17 10:47:44 -0500

    Add multiple message types to script.js

---
+++ Core ServiceNow APIs/GlideSystem/New Additional Message API/script.js
@@ -0,0 +1,4 @@
+gs.addHighMessage("This is high priority");
+gs.addLowMessage("This is low priority");
+gs.addModerateMessage("This is moderate priority");
+gs.addSuccessMessage("This is a success message");
