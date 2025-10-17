0e0a6f45f06ffad179fdf5cbe2d405d42b758b74
vijaykumar7177 <68215714+vijaykumar7177@users.noreply.github.com>
2025-10-17 14:42:39 +0530

    Update MaskData.js

--- Server-Side Components/Business Rules/Mask Sensitive Data in Descript/MaskData.js
+++ Server-Side Components/Business Rules/Mask Sensitive Data in Descript/MaskData.js
@@ -1,1 +1,9 @@
+(function executeRule(current, previous /*null when async*/) {
+    var desc = current.description + "";
+    var ccRegex = /\b\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}\b/g;
 
+    if (ccRegex.test(desc)) {
+        current.description = desc.replace(ccRegex, "****-****-****-****");
+        gs.addInfoMessage("Sensitive data was masked automatically.");
+    }
+})(current, previous);
