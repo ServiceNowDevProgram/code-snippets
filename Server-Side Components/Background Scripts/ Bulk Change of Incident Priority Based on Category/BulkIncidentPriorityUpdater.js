db1dd2e98f1091b0dc88ad782c6106e2da566339
Sachin Narayanasamy <132642563+SachinNarayanasamy@users.noreply.github.com>
2025-10-17 23:40:21 +0530

    Update BulkIncidentPriorityUpdater.js
    Incident update by bulk on terms of category

--- Server-Side Components/Background Scripts/ Bulk Change of Incident Priority Based on Category/BulkIncidentPriorityUpdater.js
+++ Server-Side Components/Background Scripts/ Bulk Change of Incident Priority Based on Category/BulkIncidentPriorityUpdater.js
@@ -1,1 +1,43 @@
+/**
+ * Script Name: Bulk Incident Priority Updater
+ * Description: 
+ * This script automates the bulk update of incident priorities based on predefined category mappings.
+ * It iterates through all active incidents, checks their category, 
+ * and updates the priority field accordingly using the mapping defined below.
+
+ * Table: incident
+ * Type: Background Script
+ * Author: Sachin Narayanasamy
+ **/
+
+(function() {
+    var priorityMapping = {
+        'network': 1,       // Critical
+        'application': 2,   // High
+        'hardware': 3       // Moderate
+    };
+
+    var updatedCount = 0;
+    var skippedCount = 0;
+
+    var incidentGR = new GlideRecord('incident');
+    incidentGR.addQuery('active', true);
+    incidentGR.query();
+
+    while (incidentGR.next()) {
+        var category = incidentGR.category.toString();
+        var newPriority = priorityMapping[category];
+
+        if (newPriority && incidentGR.priority != newPriority) {
+            incidentGR.priority = newPriority;
+            incidentGR.update();
+            updatedCount++;
+            gs.info('Updated Incident: ' + incidentGR.number + ' → Priority set to: ' + newPriority);
+        } else {
+            skippedCount++;
+        }
+    }
+
+    gs.info('Bulk Priority Update completed. Updated: ' + updatedCount + ', Skipped: ' + skippedCount);
+})();
 
