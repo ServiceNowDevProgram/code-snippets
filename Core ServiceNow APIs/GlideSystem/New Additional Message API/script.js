6d657cb0c34b88d77f7d55478a7ff3a0c4434b6e
HackoDev1530 <92592691+HackoDev1530@users.noreply.github.com>
2025-10-17 13:33:19 -0500

    Update script.js

--- Core ServiceNow APIs/GlideSystem/New Additional Message API/script.js
+++ Core ServiceNow APIs/GlideSystem/New Additional Message API/script.js
@@ -1,4 +1,8 @@
-gs.addHighMessage("This is high priority");
-gs.addLowMessage("This is low priority");
-gs.addModerateMessage("This is moderate priority");
-gs.addSuccessMessage("This is a success message");
+(function executeRule(current, previous /*null when async*/ ) {
+
+    gs.addHighMessage("This is high priority");
+    gs.addLowMessage("This is low priority");
+    gs.addModerateMessage("This is moderate priority");
+    gs.addSuccessMessage("This is a success message");
+
+})(current, previous);
