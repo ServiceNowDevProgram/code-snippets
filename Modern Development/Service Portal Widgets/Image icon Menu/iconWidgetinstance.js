56653db87d9daca963b87c411ed486f922dca02a
chaitaliKundu <kunduabira6@gmail.com>
2025-10-19 15:32:07 +0530

    Image Icon Menu Item Widget (#2286)
    * README.md
    
    * pdfletter.js
    
    * Delete Server-Side Components/Business Rules/PdfletterGeneration directory
    
    * README.md
    
    * removeuser.js
    
    * Delete Specialized Areas/Fix scripts/Delete User Groups directory
    
    * README.md
    
    * Server.js
    
    * Update README.md
    
    * client.js
    
    * Update Server.js
    
    * Update Server.js
    
    * iconmenu.html
    
    * iconmenu.css
    
    * iconWidgetinstance.js
    
    * Add files via upload
    
    * Add files via upload

---
+++ Modern Development/Service Portal Widgets/Image icon Menu/iconWidgetinstance.js
@@ -0,0 +1,53 @@
+{
+      "header_image": {
+            "value": "image_icon.png",
+            "displayValue": "image_icon.png"
+      },
+      "header_title": {
+            "value": "Quick Links",
+            "displayValue": "Quick Links"
+      },
+     
+     
+      "item_1_title": {
+            "value": "Request Knowledge Articles",
+            "displayValue": "Request Knowledge Articles"
+      },
+      "item_1_img": {
+            "value": "request_ka_icon.png",
+            "displayValue": "request_ka_icon.png"
+      },
+      "item_1_url": {
+            "value": "?id=sc_cat_item&sys_id=657498bf1b00c5d0fd4899f4bd4bcb1e",
+            "displayValue": "?id=sc_cat_item&sys_id=657498bf1b00c5d0fd4899f4bd4bcb1e"
+      },
+     
+      
+      "item_1_target": {
+            "value": "_blank",
+            "displayValue": "_blank"
+      },
+      "item_2_target": {
+            "value": "_blank",
+            "displayValue": "_blank"
+      },
+     
+    
+
+      "item_2_title": {                                                       
+            "value": "Rewards & Recognitions",
+            "displayValue": "Rewards & Recognitions"
+      },
+      "item_2_img": {
+            "value": "achv_icon.png",
+            "displayValue": "achv_icon.png"
+      },
+      "item_2_url": {
+            "value": "https://xyz.achievers.com/recent_activity",
+            "displayValue": "https://xyz.achievers.com/recent_activity"
+     
+    },
+
+     
+     
+}
