4848474a90cbfe4dd4c69899b578272a6ccfd3eb
AmrutaKR007 <amruta.rajarama@sanvartis.de>
2025-10-20 15:06:16 +0200

    Update approval.js
    Instead of hardcoding the table name . Write current.sys_class_name so that in future if we we need the business rule on change tasks. We can write the business rule on Task table.

--- Server-Side Components/Business Rules/After-BR to generate approvals for catalog tasks/approval.js
+++ Server-Side Components/Business Rules/After-BR to generate approvals for catalog tasks/approval.js
@@ -6,7 +6,7 @@ var sysApp = new GlideRecord('sysapproval_approver');
 
   sysApp.state = 'requested'; //set the state to requested
   sysApp.sysapproval = current.sys_id; // set the 'Approval for' field with the current catalog task
-  sysApp.source_table = 'sc_task'; // set the source table field so that document ID table can be updated properly as it is dependent on source table field.
+  sysApp.source_table = current.sys_class_name; // set the source table field so that document ID table can be updated properly as it is dependent on source table field.
   sysApp.approver = 'sys_id of the person for whom you want to trigger this approval'; // set the approver
   sysApp.document_id = current.sys_id; //set the sys_id to populate correct value in the Approving field.
   
