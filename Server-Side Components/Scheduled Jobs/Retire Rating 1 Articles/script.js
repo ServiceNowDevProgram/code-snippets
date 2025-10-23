3c894e5e96cbcb7fa221de4ea1666b86ec982ce4
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-23 23:24:29 +0530

    Update script.js

--- Server-Side Components/Scheduled Jobs/Retire Rating 1 Articles/script.js
+++ Server-Side Components/Scheduled Jobs/Retire Rating 1 Articles/script.js
@@ -6,15 +6,15 @@ This Job will look for articles which have been rated 1 three times and will ret
 var kbFeed = new GlideAggregate('kb_feedback');
 kbFeed.addEncodedQuery('rating=1'); // get articles with rating 1.
 kbFeed.addAggregate('COUNT', 'article');
-kbFeed.groupBy('user');
+kbFeed.groupBy('article');
 kbFeed.query();
 while (kbFeed.next()) {
 
-    if (kbFeed.getAggregate('COUNT', 'article') >= 3) {
+    if (kbFeed.getAggregate('COUNT', 'article') >= 3) { // check if article is rated 1 three times
         var updateArt = new GlideRecord('kb_knowledge');
         updateArt.get(kbFeed.article.sys_id);
-        if (updateArt.workflow_state == 'published') {
-            updateArt.workflow_state = 'retired';
+        if (updateArt.workflow_state == 'published') {   //check if article is published
+            updateArt.workflow_state = 'retired';  // retire the article
             updateArt.update();
         }
     }
