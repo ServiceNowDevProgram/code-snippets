6f69cccd228f9f0719e4da1e66202f3fc0621c4f
Chaitanya Lal <42643661+chaitanyalal18@users.noreply.github.com>
2025-10-24 12:14:21 +0530

    Update Script.js
    As per the feedback from PR

--- Server-Side Components/Business Rules/Move attachment from variable to record/Script.js
+++ Server-Side Components/Business Rules/Move attachment from variable to record/Script.js
@@ -1,18 +1,29 @@
 (function executeRule(current, previous /*null when async*/) {
 
-    // Find attachments linked to variables (table_sys_id of RITM's variables)
-    var attGR = new GlideRecord('sys_attachment');
-    attGR.addQuery('table_sys_id', current.sys_id); // attachment originally linked to variable of this RITM
-    attGR.addQuery('table_name', '!=', 'sc_req_item'); // exclude already moved attachments
-    attGR.query();
+    // List of variable names for which the attachment has to be moved to the record level.
+    var attachmentVars = ['attachment1', 'attachment2']; 
+    
+    for (var i = 0; i < attachmentVars.length; i++) {
+        var varName = attachmentVars[i];
 
-    while (attGR.next()) {
-        gs.info('Moving attachment: ' + attGR.file_name);
+        // Get the attachment sys_id from variable value
+        var attachmentSysId = current.variables[varName];
 
-        // Update to associate with the RITM record
-        attGR.table_name = 'sc_req_item';
-        attGR.table_sys_id = current.sys_id;
-        attGR.update();
+        if (!attachmentSysId) {
+            gs.info("No attachment found in variable: " + varName);
+            continue;
+        }
+
+        // Get attachment record using sys_id
+        var attGR = new GlideRecord('sys_attachment');
+        if (attGR.get(attachmentSysId)) {
+            gs.info('Moving attachment: ' + attGR.file_name);
+
+            // Update reference to link to the RITM
+            attGR.table_name = 'sc_req_item';
+            attGR.table_sys_id = current.sys_id;
+            attGR.update();
+        }
     }
 
 })(current, previous);
