2ca888c879ea6eac4fc89433aa6e7eaecbb26da2
sanjaykumar3sn <gupta.sanjaykumar@bcg.com>
2025-10-25 23:29:00 +0100

    Create Weekly_LicensedUser_Access_Revoke_90Days.js

---
+++ Server-Side Components/Scheduled Jobs/Licensed User Access Job/Weekly_LicensedUser_Access_Revoke_90Days.js
@@ -0,0 +1,44 @@
+(function executeWeeklyJob() {
+
+    var DAYS_INACTIVE_THRESHOLD = 90; // number of days
+    var licensedRoles = ['itil', 'sys_approver', 'admin', 'business_stakeholder'];
+  
+    var roleGroupMap = {
+        'itil': 'ITIL Group',
+        'sys_approver': 'Approver Group',
+        'admin': 'Admin Group',
+        'business_stakeholder': 'Business Stakeholder Group'
+    };
+
+    var thresholdDate = new GlideDateTime();
+    thresholdDate.addDaysUTC(-DAYS_INACTIVE_THRESHOLD);
+
+    for (var i = 0; i < licensedRoles.length; i++) {
+        var role = licensedRoles[i];
+        var groupName = roleGroupMap[role];
+
+        var userRoleGR = new GlideRecord('sys_user_has_role');
+        userRoleGR.addQuery('role.name', role);
+        userRoleGR.addQuery('user.active', true);
+        userRoleGR.query();
+
+        while (userRoleGR.next()) {
+            var user = userRoleGR.user.getRefRecord();
+            var lastLogin = user.last_login_time;
+
+            // user never logged in or inactive beyond threshold
+            if (!lastLogin || lastLogin < thresholdDate) {
+                gs.info('Revoking access for user: ' + user.name + ' (' + role + ')');
+
+                // Remove from group
+                var groupGR = new GlideRecord('sys_user_grmember');
+                groupGR.addQuery('user', user.sys_id);
+                groupGR.addQuery('group.name', groupName);
+                groupGR.query();
+                while (groupGR.next()) {
+                    groupGR.deleteRecord();
+                }
+            }
+        }
+    }
+})();
