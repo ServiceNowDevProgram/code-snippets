0df8adac460761ba81d5c3728eb4c7db7386d641
SUMANTH GOLLA <113332783+sumanth1710@users.noreply.github.com>
2025-10-26 16:18:46 +0530

    Create IncFieldDecorator.js

---
+++ Client-Side Components/Catalog Client Script/Readme.md/IncFieldDecorator.js
@@ -0,0 +1,6 @@
+function onLoad() {
+   //Type appropriate comment here, and begin script below
+   g_form.addDecoration('caller_id', 'icon-star', 'Mark as Favorite', 'color-blue');
+g_form.addDecoration('category' , 'icon-lightbulb' , 'icon-lightbulb' , 'color-yellow');
+
+}
