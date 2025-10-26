63388bb12c21d13d4658ea66d6c219face82b543
NayeemKaka <nayeemind@gmail.com>
2025-10-26 12:33:42 +0530

    Update Client Script.js

--- Modern Development/Service Portal Widgets/Sticky Notes/Client Script.js
+++ Modern Development/Service Portal Widgets/Sticky Notes/Client Script.js
@@ -1,20 +1,19 @@
 api.controller=function($scope) {
 /* widget controller */
 var c = this;
-c.add =function(){
-c.data.action = "addMessage";
-c.server.update().then(function(){
-c.data.action = undefined;
-	c.data.newColor ="";
-c.data.message = "";
-})
-}
-c.remove =function(i){
-c.data.i =i;
-c.data.action = "removeMessage";
-c.server.update().then(function(){
-c.data.action = undefined;
-})
-
-}
+ c.add =function(){
+  c.data.action = "add";
+  c.server.update().then(function(){
+    c.data.action = undefined;
+	  c.data.newColor ="";
+    c.data.text = "";
+  })
+ }
+ c.remove =function(i){
+  c.data.i =i;
+  c.data.action = "remove";
+  c.server.update().then(function(){
+    c.data.action = undefined;
+  })
+ }
 }
