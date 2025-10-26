338c408605f5310a98595c4e877a00ae99bd2205
NayeemKaka <nayeemind@gmail.com>
2025-10-26 12:34:19 +0530

    Update Server Side.js

--- Modern Development/Service Portal Widgets/Sticky Notes/Server Side.js
+++ Modern Development/Service Portal Widgets/Sticky Notes/Server Side.js
@@ -1,34 +1,25 @@
 (function() {
-data.messages = [];
 if(input){
-data.messages = input.messages;
-if (input.action == "addMessage") {
-data.messages.push(input.message);
-	if(!input.message){
+  if (input.action == "add") {
+  	if(!input.text){
 		gs.addInfoMessage("Please enter value in text area");
 		return;
 	}
-	   var rec = new GlideRecord('u_sticky_notes'); // 🔹 change this to your table
-      rec.initialize();
-			rec.u_short_description = input.message;
-			rec.u_color = input.newColor;
-    //  rec.setValue('u_short_description', input.inputValue); // 🔹 change 'name' to your field
-      rec.insert();
-	
-	
-}
-if (input.action == "removeMessage") {
-data.messages.splice(input.i,1);
-	console.log(input.i);
-	 var del = new GlideRecord('u_sticky_notes');
+	     var rec = new GlideRecord('u_sticky_notes'); 
+       rec.initialize();
+		   rec.u_short_description = input.text;
+			 rec.u_color = input.newColor;
+        rec.insert();
+	}
+  if (input.action == "remove") {
+	   var del = new GlideRecord('u_sticky_notes');
         if (del.get(input.i)) {
           del.deleteRecord();
-      
         }
-}
+  }
 }
 	
-		var gr = new GlideRecord('u_sticky_notes');
+	var gr = new GlideRecord('u_sticky_notes');
 	gr.orderByDesc('sys_created_on');
 	gr.query();
 	data.notes = [];
