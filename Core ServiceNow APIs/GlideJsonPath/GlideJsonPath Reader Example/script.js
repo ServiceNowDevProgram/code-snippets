59ba7f08ba1b56db997ed928f0d4b5b57f34e57e
trimbakeshmadhan-109 <trimbakeshmadhan@gmail.com>
2025-10-28 03:21:18 +0530

    Update script.js

--- Core ServiceNow APIs/GlideJsonPath/GlideJsonPath Reader Example/script.js
+++ Core ServiceNow APIs/GlideJsonPath/GlideJsonPath Reader Example/script.js
@@ -1,3 +1,3 @@
-var v = new GlideJsonPath('{"lib":{"jsonpath":{"cricket":{"name":"India","players":["Rohit","Dhoni","Kholi"]}}}}'); 
+var v = new GlideJsonPath('{"lib":{"jsonpath":{"cricket":{"name":"India","players":["Rohit sharma","Dhoni","Kholi"]}}}}'); 
 var l = v.read("$['lib']['jsonpath']['cricket']['players'][*]");
 
