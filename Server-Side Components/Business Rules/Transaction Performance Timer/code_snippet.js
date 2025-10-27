9e4157b641b3098000628b3fe711429ae0e45a77
Prashant Kumar <learnitbyprashant@gmail.com>
2025-10-27 11:59:04 +0530

    code_snippet.js

--- Server-Side Components/Business Rules/Transaction Performance Timer/code_snippet.js
+++ Server-Side Components/Business Rules/Transaction Performance Timer/code_snippet.js
@@ -1,7 +1,6 @@
 // This This code_snippet.js script measure execution time of a Business Rules.
 var start = new GlideDateTime();
 gs.info('Process started at: ' + start);
-doSomeLogic();
 var end = new GlideDateTime();
 var diff = GlideDateTime.subtract(end, start);
 gs.info('Execution took: ' + diff.getDisplayValue());
