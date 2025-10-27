ee788f1e480cdec49f898e8ba10ebf544c2c11f6
Prashant Kumar <learnitbyprashant@gmail.com>
2025-10-27 20:34:43 +0530

    code_snippet.js

---
+++ Server-Side Components/Business Rules/Auto-assign Incident to Last Engineer Who Worked on CI/code_snippet.js
@@ -0,0 +1,9 @@
+// This script auto-assign Incident to Last Engineer Who Worked on CI.
+// Before Business Rule on incident
+(function(current, previous){
+  var hist = new GlideRecord('incident');
+  hist.addQuery('cmdb_ci', current.cmdb_ci);
+  hist.orderByDesc('resolved_at');
+  hist.query();
+  if (hist.next()) current.assigned_to = hist.resolved_by;
+})(current, previous);
