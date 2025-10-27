f942d1f52775c2a9741ede0bc583e577fb1bd37f
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-27 22:49:08 +0530

    Update favCopy.js

--- Specialized Areas/Fix scripts/Copy favourite to other users/favCopy.js
+++ Specialized Areas/Fix scripts/Copy favourite to other users/favCopy.js
@@ -1,8 +1,11 @@
 var srcFav = ""; // Add the sys_id of the favourite to copy, from sys_ui_bookmark table
+var portalFav = ""; // Add the sys_id of the favourite to copy, from sp_favorite table
 var userCriteria = ""; // Add an encoded query of users from sys_user table
-
 var fav = new GlideRecord("sys_ui_bookmark");
 fav.get(srcFav);
+
+var portalFav = new GlideRecord("sp_favorite");
+portalFav.get(portalFav); 
 
 var users = new GlideRecord("sys_user");
 users.addEncodedQuery(userCriteria);
@@ -18,4 +21,17 @@ while(users.next()) {
   newFav.setValue("url",fav.url);
   newFav.setValue("user",users.sys_id);
   newFav.insert();
+
+  createPortalFav(users);
 }
+function createPortalFav(userRec){
+  var newPortalFav = new GlideRecord("sp_favorite");
+  newPortalFav.initialize();
+  newPortalFav.setValue('user',portalFav.user);
+  newPortalFav.setValue('reference_table',portalFav.reference_table);
+  newPortalFav.setValue('reference_document',portalFav.reference_document);
+  newPortalFav.insert();
+}
+
+
+
