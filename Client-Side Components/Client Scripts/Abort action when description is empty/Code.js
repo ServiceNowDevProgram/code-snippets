83820f2dce084f9c0002b01487b987d808ad928b
chaytarak <62869669+chaytarak@users.noreply.github.com>
2025-10-28 23:08:35 +0530

    Update Code.js

--- Client-Side Components/Client Scripts/Abort action when description is empty/Code.js
+++ Client-Side Components/Client Scripts/Abort action when description is empty/Code.js
@@ -1,9 +1,9 @@
 function onSubmit() {
     //Type appropriate comment here, and begin script below
     var description = g_form.getValue('description');
-	var status = g_form.getValue('status');
+	var status = g_form.getValue('state');
 
-    if ((!description) || (status == 'completed')) {
+    if ((!description) && (state == 'completed')) {
         g_form.addErrorMessage('Please provide Description Value, Description Cannot be empty');
 
         return false;
