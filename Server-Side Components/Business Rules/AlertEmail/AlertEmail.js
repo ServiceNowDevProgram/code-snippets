fe3a96f801264d81c860d4b854f1ffd331791a83
NavyasreeBolleboyana <156333434+NavyasreeBolleboyana@users.noreply.github.com>
2025-10-28 10:54:19 +0530

    AlertEmail.js

---
+++ Server-Side Components/Business Rules/AlertEmail/AlertEmail.js
@@ -0,0 +1,19 @@
+ 
+ 	
+#BusinessRule
+(function executeRule(current, previous /*null when async*/) {
+
+        // Add your code here
+ var userEmail=current.caller_id.email;
+ g_scratchpad.callerEmail=userEmail;
+})(current, previous);
+
+
+#ClientScript
+function onLoad() {
+   //Type appropriate comment here, and begin script below
+
+   var mail=g_scratchpad.callerEmail;
+   alert("callers email address : "+mail);
+   
+}
