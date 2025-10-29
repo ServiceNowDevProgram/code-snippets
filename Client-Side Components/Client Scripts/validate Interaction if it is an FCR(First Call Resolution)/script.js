5a0e77c041a4f300ebda106862b63533fc1551e9
ReddySurendra <130588276+Suri123789@users.noreply.github.com>
2025-10-29 16:56:43 +0530

    Update script.js

--- Client-Side Components/Client Scripts/validate Interaction if it is an FCR(First Call Resolution)/script.js
+++ Client-Side Components/Client Scripts/validate Interaction if it is an FCR(First Call Resolution)/script.js
@@ -1,29 +1,23 @@
-// Business Rule: Set Resolution SLA on Related Tasks
-// Table: task_sla
-// When: After Insert
+//Client Script to validate an Interaction record is resolved with out any related record created.
+function onSubmit() {
+    var relatedTask = g_form.getValue('u_boolean_no_related_task');
+    var state = g_form.getValue('state');
+    var type = g_form.getValue('type');
+    var workNotes = g_form.getValue('work_notes'); // Get the value of work notes
 
-// PURPOSE:
-// When a new Task SLA is created for Resolution SLA, update the related records.
+    // Clear previous field messages
+    g_form.clearMessages();
 
-// CONDITIONS:
-// SLA Definition target = "Resolution"
-// Run only if task is Incident or SC Task
-// (current.task.sys_class_name == 'incident' || current.task.sys_class_name == 'sc_task')
-
-// STEP 1:
-// Get the related Incident record from current.task reference
-var inc = new GlideRecord('incident'); // Query Incident table
-inc.get(current.task); // Fetch the matching incident record
-
-// Update Incident’s custom field to store the Resolution SLA reference
-inc.u_resolution_sla = current.sys_id; // Set current task_sla sys_id
-inc.update(); // Save changes to Incident
-
-// STEP 2:
-// Get the related Task record from current.task reference
-var tsk = new GlideRecord('task'); // Query Task table
-tsk.get(current.task); // Fetch the task (incident/sc_task/etc.)
-
-// Update Task’s custom field to store the Resolution SLA reference
-tsk.u_task_resolution_sla = current.sys_id; // Set current task_sla sys_id
-tsk.update(); // Save changes to Task
+    // Check if state is changing to 'Closed Complete'
+    if (state == 'closed_complete') {
+        // Check additional conditions
+        if (type == 'walkup' && relatedTask == 'true') {
+            return true; // Allow form submission
+        } else if (!workNotes) { // Check if work notes is empty
+            g_form.showFieldMsg('work_notes', 'Provide Worknotes for FCR Interaction', 'error');
+			alert('Provide Worknotes for FCR Interaction');
+            return false; // Prevent form submission
+        }
+    }
+    return true; // Allow form submission for other states
+}
