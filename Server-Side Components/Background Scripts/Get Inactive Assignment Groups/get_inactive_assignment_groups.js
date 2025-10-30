31ef644969d620bbc476dd45c7f36cf030a9a973
anjimuvva <42597425+anjimuvva@users.noreply.github.com>
2025-10-30 18:49:41 +0800

    Update get_inactive_assignment_groups.js

--- Server-Side Components/Background Scripts/Get Inactive Assignment Groups/get_inactive_assignment_groups.js
+++ Server-Side Components/Background Scripts/Get Inactive Assignment Groups/get_inactive_assignment_groups.js
@@ -1,11 +1,21 @@
+var activeGroups = {};
+var member = new GlideAggregate('sys_user_grmember');
+member.addQuery('user.active', true);
+member.groupBy('group');
+member.query();
+while (member.next()) {
+    activeGroups[member.group.toString()] = true;
+}
+
+var inactiveCount = 0;
 var grp = new GlideRecord('sys_user_group');
+grp.addQuery('active', true); // optional filter
 grp.query();
 while (grp.next()) {
-    var member = new GlideRecord('sys_user_grmember');
-    member.addQuery('group', grp.sys_id);
-    member.addQuery('user.active', true);
-    member.query();
-    if (!member.hasNext()) {
+    if (!activeGroups[grp.sys_id.toString()]) {
         gs.info('Inactive group (no active members): ' + grp.name);
+        inactiveCount++;
     }
 }
+
+gs.info('Total inactive groups found: ' + inactiveCount);
