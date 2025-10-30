b5a54acf82823d91cbed01bdb5b49b1369d6f621
NayeemKaka <nayeemind@gmail.com>
2025-10-30 21:16:15 +0530

    Update script.js

--- Integration/RESTMessageV2/Exchange Rates Integration/script.js
+++ Integration/RESTMessageV2/Exchange Rates Integration/script.js
@@ -1,1 +1,22 @@
+var symbols ="INR,EUR,KWD,SAR"; //Enter symbol name like SAR,AED
+var apiKey =""; // Paste your CurrencyFreaks APIKEY here
+getExchangeReate(apiKey, symbols);
 
+function getExchangeReate(apiKey,symbols){
+	try { 
+	
+ var r = new sn_ws.RESTMessageV2('ExchangeRate API', 'Default GET');
+ r.setStringParameterNoEscape('symbols', symbols);
+ r.setStringParameterNoEscape('apikey', apiKey);
+
+
+ var response = r.execute();
+ var responseBody = response.getBody();
+ var httpStatus = response.getStatusCode();
+ gs.print("Status: " +httpStatus);
+ gs.print("Result:" +responseBody); //It will show conversion from USD the selected currency 
+}
+catch(ex) {
+ var message = ex.message;
+}
+}
