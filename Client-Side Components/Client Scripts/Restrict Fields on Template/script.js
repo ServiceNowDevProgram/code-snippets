e7aa2f92cd96a25c997234665ce836eb46657d92
Raghav Sharma <53517312+raghavs046@users.noreply.github.com>
2025-10-31 19:40:12 +0530

    Restrict fields on template (#2659)
    * Add script to display user mentions in Service Portal
    
    This script retrieves and displays the top 5 records where the logged-in user is mentioned, providing links to those records.
    
    * Delete Modern Development/Service Portal Widgets/My Mentioned Items directory
    
    * Create script.js
    
    * Delete Modern Development/Service Portal Widgets/My Mentioned Items directory
    
    * Create script.js
    
    * Delete Server-Side Components/Scheduled Jobs/Retire Rating 1 Articles directory
    
    * Create script.js
    
    * Update script.js
    
    * Create README.md
    
    * Update README.md
    
    * Update script.js

---
+++ Client-Side Components/Client Scripts/Restrict Fields on Template/script.js
@@ -0,0 +1,20 @@
+/*
+Type: onChnage
+Table: sys_template
+Field: Template
+*/
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+    if (isLoading || newValue === '') {
+        return;
+    }
+    if (g_form.getValue('table') == 'incident') { // table on which sys_template is being created.
+        var fields = ['active', 'comments']; // array of fields to be restricted while template creation.
+        for (var i = 0; i < fields.length; i++) {
+            if (newValue.indexOf(fields[i]) > -1) {
+                alert("You Cannot Add " + fields[i]); // alert if user selects the restricted field.
+                var qry = newValue.split(fields[i]);
+                g_form.setValue('template', qry[0] + 'EQ');  // set the template value to previous values (oldValue does not work in this case).
+            }
+        }
+    }
+}
